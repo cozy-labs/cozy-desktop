a41b987e242768bcd0f1a06a592bd92ad750c7c5
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-10-09 12:06:51 +0200

    add build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var shell = require('gulp-shell');
+var zip = require('gulp-zip');
+var concat = require('gulp-concat');
 var del = require('del');
 
+var nwVersion = '0.8.6';
 var paths = {
   scripts: ['backend/*.coffee'],
-  scriptsJS: ['backend/*.js']
+  scriptsJS: ['backend/*.js'],
+  all: ["backend/**/*.js", "client/public/**", "app.html", "package.json",
+        "node_modules/**"],
+  leveldown: 'node_modules/pouchdb/node_modules/leveldown'
 };
+
 
 gulp.task('clean', function(cb) {
   del(paths.scriptsJS, cb);
@@ -20,5 +28,26 @@ gulp.task('scripts', ['clean'], function() {
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
 });
+
+gulp.task('leveldown', shell.task([
+  'cd ' + paths.leveldown + ' && nw-gyp configure --target=' + nwVersion,
+  'cd ' + paths.leveldown + ' && nw-gyp build'
+]));
+
+gulp.task('builder', function() {
+  var NwBuilder = require('node-webkit-builder');
+  var nw = new NwBuilder({
+      files: paths.all,
+      version: nwVersion,
+      platforms: ['linux64']
+  });
+  nw.build().then(function () {
+     console.log('Cozy Data Proxy was successfully built.');
+  }).catch(function (error) {
+     console.log('An error occured whild building Cozy Data Proxy.');
+     console.log(error);
+  });
+});
+
 
 gulp.task('default',  ['watch']);
