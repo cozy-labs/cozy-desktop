25a65e9f6e363612911f1da8ce9bad27a6054a7a
Bruno Michel <bmichel@menfin.info>
2016-03-18 12:56:26 +0100

    Add cozy-desktop

--- gui/main.js
+++ gui/main.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const desktop = require('cozy-desktop')
 const electron = require('electron')
 const path = require('path')
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+
+console.log(desktop)
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
