fad4c9d8aa921e03edaafeae2b18a6801095e099
Bruno Michel <bmichel@menfin.info>
2016-03-18 17:46:13 +0100

    Start cozy-desktop from GUI

--- gui/main.js
+++ gui/main.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const desktop = require('cozy-desktop')
+const Desktop = require('cozy-desktop')
 const electron = require('electron')
 const path = require('path')
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-
-console.log(desktop)
+const ipcMain = electron.ipcMain
+const desktop = new Desktop(process.env.COZY_DESKTOP_DIR)
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -26,7 +26,8 @@ app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    console.log('Exiting...')
+    desktop.stopSync(() => { app.quit() })
   }
 })
 
@@ -36,4 +37,21 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
+})
+
+ipcMain.on('add-remote', (event, arg) => {
+  console.log('arg', arg)
+  desktop.askPassword = (cb) => {
+    console.log('askPassword', arg.password)
+    cb(null, arg.password)
+  }
+  desktop.addRemote(arg.url, arg.folder, null, (err) => {
+    event.sender.send('remote-added', err)
+    desktop.synchronize('full', (err) => {
+      if (err) {
+        console.log(err)
+        app.quit()
+      }
+    })
+  })
 })
