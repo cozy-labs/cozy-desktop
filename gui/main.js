1e1a66d4b35fc58b079b2ac4f06b4cc5851e8813
Bruno Michel <bmichel@menfin.info>
2016-03-21 18:15:58 +0100

    Make it run as a service

--- gui/main.js
+++ gui/main.js
@@ -6,37 +6,53 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipcMain = electron.ipcMain
+const nativeImage = electron.nativeImage
 const desktop = new Desktop(process.env.COZY_DESKTOP_DIR)
+
+// Use a fake window to keep the application running when the main window is
+// closed: it runs as a service, with a tray icon if you want to quit it
+let runAsService
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let tray
+
+const windowOptions = {
+  width: 1024,
+  height: 768,
+  icon: `${__dirname}/images/icon.png`,
+  skipTaskbar: true,
+  closable: false
+}
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 1024, height: 768 })
+  runAsService = new BrowserWindow({ show: false })
+  mainWindow = new BrowserWindow(windowOptions)
   mainWindow.loadURL(`file://${__dirname}/index.html`)
   if (process.env.WATCH === 'true') {
-    console.log('WATCH')
     mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.setMenu(null)
   }
   mainWindow.on('closed', () => { mainWindow = null })
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    console.log('Exiting...')
-    desktop.stopSync(() => { app.quit() })
-  }
+app.on('ready', () => {
+  createWindow()
+  tray = new electron.Tray(`${__dirname}/images/icon.png`)
+  const menu = electron.Menu.buildFromTemplate([
+    { label: 'Quit', click: app.quit }
+  ])
+  tray.setContextMenu(menu)
 })
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (mainWindow) {
+    mainWindow.focus()
+  } else {
     createWindow()
   }
 })
