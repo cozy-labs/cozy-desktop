b225ea3427c5e9c241e34dd9e8ab69dd88496ebb
Bruno Michel <bmichel@menfin.info>
2016-03-23 17:01:13 +0100

    Move to another screen when the wizard is finished

--- gui/lib/Main.elm
+++ gui/lib/Main.elm
@@ -5,20 +5,70 @@ import Html exposing (Html)
 import Effects exposing (Effects, Never)
 import Task exposing (Task)
 import Wizard
+import TwoPanes
 
 
-init : ( Wizard.Model, Effects Wizard.Action )
+-- MODEL
+
+
+type Page
+  = WizardPage
+  | TwoPanesPage
+
+
+type alias Model =
+  { page : Page
+  , wizard : Wizard.Model
+  , twopanes : TwoPanes.Model
+  }
+
+
+init : ( Model, Effects Wizard.Action )
 init =
-  ( Wizard.init, Effects.none )
+  let
+    model =
+      Model WizardPage Wizard.init TwoPanes.init
+  in
+    ( model, Effects.none )
 
 
-app : StartApp.App Wizard.Model
+
+-- UPDATE
+
+
+update : Wizard.Action -> Model -> ( Model, Effects Wizard.Action )
+update action model =
+  case action of
+    Wizard.StartSync ->
+      ( { model | page = TwoPanesPage }, Effects.none )
+
+    _ ->
+      let
+        ( wizard', effects ) =
+          Wizard.update action model.wizard
+      in
+        ( { model | wizard = wizard' }, effects )
+
+
+
+-- VIEW
+
+
+view : Signal.Address Wizard.Action -> Model -> Html
+view address model =
+  if model.page == WizardPage then
+    Wizard.view address model.wizard
+  else
+    TwoPanes.view address model.twopanes
+
+
+app : StartApp.App Model
 app =
   StartApp.start
     { init = init
     , inputs = []
-    , update = Wizard.update
-    , view = Wizard.view
+    , update = update
+    , view = view
     }
 
 
--- gui/lib/TwoPanes.elm
+++ gui/lib/TwoPanes.elm
@@ -2,11 +2,48 @@ module TwoPanes (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Models exposing (AppModel)
-import Update exposing (..)
+import Effects exposing (Effects)
+import Wizard
 
 
-view : Signal.Address Action -> AppModel -> Html
+-- MODEL
+
+
+type Tab
+  = DashboardTab
+  | SettingsTab
+  | AccountTab
+  | HelpTab
+
+
+type alias Model =
+  { tab : Tab }
+
+
+init : Model
+init =
+  { tab = DashboardTab }
+
+
+
+-- UPDATE
+
+
+type Action
+  = NoOp
+
+
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+  case action of
+    _ ->
+      ( model, Effects.none )
+
+
+
+-- VIEW
+
+
+view : Signal.Address Wizard.Action -> Model -> Html
 view address model =
   div [ id "two-panes" ] [ text "Work in progress" ]
