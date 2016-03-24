22841a7ad601755d7ab5e894b2bf6aca39d98f79
Bruno Michel <bmichel@menfin.info>
2016-03-24 14:29:37 +0100

    Open external links in browser

--- gui/ports.js
+++ gui/ports.js
@@ -1,9 +1,18 @@
 'use strict'
 /* global Elm */ // This will keep your linter happy
 
+const shell = require('electron').shell
 const ipcRenderer = require('electron').ipcRenderer
 
 const container = document.getElementById('container')
 const elmectron = Elm.embed(Elm.Main, container)
 
 console.log(elmectron.ports, ipcRenderer)
+
+// Open external links in the browser
+document.addEventListener('click', (event) => {
+  if (event.target.matches('a[href^="http"]')) {
+    event.preventDefault()
+    shell.openExternal(event.target.href)
+  }
+})
