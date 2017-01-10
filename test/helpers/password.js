6c76cdae19df3312abeba9e82cb4d25377abb1f3
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-01-10 16:59:33 +0100

    build: Use node-friendly syntax in test password module
    So it can also be used in scripts, including Travis:
    - Use strict mode (needed for let statements)
    - Use module.exports (export statement isn't supported natively)
    - Passes eslint check

--- test/helpers/password.js
+++ test/helpers/password.js
@@ -1,9 +1,11 @@
+'use strict'
+
 // The password used for integration tests:
 let password = 'CozyTest$1'
 
 // You can retrieve it by requiring this module:
 //     password = require '.../password'
-export default password
+module.exports = password
 
 // Running this script directly with the `coffeescript` command prints the
 // password to the console (useful for scripting, e.g. on Travis).
