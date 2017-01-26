5668a25abedd96ee0b641116250af4a3a3cc24da
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-01-26 11:36:09 +0100

    remote/cozy: Prepare unit test for new test cases

--- test/unit/remote/cozy.js
+++ test/unit/remote/cozy.js
@@ -7,34 +7,36 @@ import { COZY_URL, builders } from '../../helpers/integration'
 
 describe('RemoteCozy', function () {
   describe('changes', function () {
-    let remoteCozy
+    context('when cozy works', function () {
+      let remoteCozy
 
-    beforeEach(function () {
-      remoteCozy = new RemoteCozy(COZY_URL)
-    })
-
-    context('without an update sequence', function () {
-      it('lists all changes since the database creation', async function () {
-        let changes = await remoteCozy.changes()
-        let ids = changes.results.map(result => result.id)
-
-        ids.should.containEql(ROOT_DIR_ID)
-        ids.should.containEql(TRASH_DIR_ID)
+      beforeEach(function () {
+        remoteCozy = new RemoteCozy(COZY_URL)
       })
-    })
 
-    context('with an update sequence', function () {
-      it('lists only changes that occured since then', async function () {
-        let oldChanges = await remoteCozy.changes()
-        let seq = oldChanges.last_seq
+      context('without an update sequence', function () {
+        it('lists all changes since the database creation', async function () {
+          let changes = await remoteCozy.changes()
+          let ids = changes.results.map(result => result.id)
 
-        let dir = await builders.dir(remoteCozy).build()
-        let file = await builders.file(remoteCozy).inDir(dir).build()
+          ids.should.containEql(ROOT_DIR_ID)
+          ids.should.containEql(TRASH_DIR_ID)
+        })
+      })
 
-        let newChanges = await remoteCozy.changes(seq)
-        let ids = newChanges.results.map(result => result.id).sort()
+      context('with an update sequence', function () {
+        it('lists only changes that occured since then', async function () {
+          let oldChanges = await remoteCozy.changes()
+          let seq = oldChanges.last_seq
 
-        ids.should.eql([file._id, dir._id].sort())
+          let dir = await builders.dir(remoteCozy).build()
+          let file = await builders.file(remoteCozy).inDir(dir).build()
+
+          let newChanges = await remoteCozy.changes(seq)
+          let ids = newChanges.results.map(result => result.id).sort()
+
+          ids.should.eql([file._id, dir._id].sort())
+        })
       })
     })
   })
