bebff7a52c2e5ce875f0c2841a2582491e8eb0b1
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-01-26 11:46:51 +0100

    remote/cozy: Add tests for cozy-stack failures

--- test/unit/remote/cozy.js
+++ test/unit/remote/cozy.js
@@ -1,12 +1,39 @@
 /* eslint-env mocha */
 
+import { FetchError } from 'node-fetch'
+
 import RemoteCozy from '../../../src/remote/cozy'
 import { ROOT_DIR_ID, TRASH_DIR_ID } from '../../../src/remote/constants'
 
 import { COZY_URL, builders } from '../../helpers/integration'
+import CozyStackDouble from '../../doubles/cozy_stack'
+
+const cozyStackDouble = new CozyStackDouble()
 
 describe('RemoteCozy', function () {
+  before(() => cozyStackDouble.start())
+  after(() => cozyStackDouble.stop())
+  afterEach(() => cozyStackDouble.clearStub())
+
   describe('changes', function () {
+    it('rejects when Cozy is unreachable', function () {
+      const url = 'http://unreachable.cozy.test'
+      const remoteCozy = new RemoteCozy(url)
+
+      return remoteCozy.changes().should.be.rejectedWith(FetchError)
+    })
+
+    it('rejects when cozy sends invalid JSON', function () {
+      const remoteCozy = new RemoteCozy(cozyStackDouble.url())
+
+      cozyStackDouble.stub((req, res) => {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.end('Not Found')
+      })
+
+      return remoteCozy.changes().should.be.rejectedWith(SyntaxError)
+    })
+
     context('when cozy works', function () {
       let remoteCozy
 
