dfb0858a49a762d18eb1b1d2cf7fb358d91526d5
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-02-03 17:24:32 +0100

    remote/constants: Add file and directory document types

--- src/remote/constants.js
+++ src/remote/constants.js
@@ -1,6 +1,12 @@
 // https://github.com/cozy/cozy-stack/blob/master/pkg/consts/consts.go
 
+// Doctypes
 export const FILES_DOCTYPE = 'io.cozy.files'
 
+// Files document type
+export const DIR_TYPE = 'directory'
+export const FILE_TYPE = 'file'
+
+// Special document ids
 export const ROOT_DIR_ID = 'io.cozy.files.root-dir'
 export const TRASH_DIR_ID = 'io.cozy.files.trash-dir'
