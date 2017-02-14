2eda94be4edaec5cda9e3a122f82d354143bcd18
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-02-14 18:15:41 +0100

    test: Upload fixture files using streams
    - We don't need to load everything in a buffer anymore.
    - We don't need to monkey-path cozy-client-js anymore.
    - We should be able to bring back node 7 build now.

--- test/helpers/cozy.js
+++ test/helpers/cozy.js
@@ -22,18 +22,6 @@ cozy._authcreds = Promise.resolve({
   }
 })
 
-// FIXME: Temporary hack to prevent cozy-client-js to convert node Buffers
-// into node-fetch-unsupported ArrayBuffers, until we implement Stream support
-// in cozy-client-js.
-const origCozyFilesCreate = cozy.files.create
-cozy.files.create = function (...args) {
-  const origArrayBuffer = global.ArrayBuffer
-  global.ArrayBuffer = Buffer
-  const result = origCozyFilesCreate(...args)
-  global.ArrayBuffers = origArrayBuffer
-  return result
-}
-
 // Facade for all the test data builders
 export const builders = new BuilderFactory(cozy)
 
