905cf5ce41d817a79f1709b37172d0bb25711739
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-02-16 12:42:21 +0100

    file_stream_provider: Add createReadStreamAsync()

--- src/file_stream_provider.js
+++ src/file_stream_provider.js
@@ -1,10 +1,13 @@
 /* @flow */
 
+import * as stream from 'stream'
+
 import type { Metadata } from './metadata'
 import type { Callback } from './utils'
 
 // Provides a stream.Readable for local or remote file corresponding to the
 // given metadata.
 export type FileStreamProvider = {
-  createReadStream: (Metadata, Callback) => void
+  createReadStream: (Metadata, Callback) => void;
+  createReadStreamAsync: (Metadata) => Promise<stream.Readable>;
 }
