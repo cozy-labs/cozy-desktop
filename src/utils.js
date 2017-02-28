91d0a1e7a365445dfb9976ba4377e903ef2a4a3d
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-02-28 13:26:14 +0100

    utils: Introduce composeAsync()

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,11 @@
 // FIXME: Could not find a way to define a Callback<T> type with multiple
 //        signatures.
 export type Callback = (?Error, any) => void;
+
+export function composeAsync (f1: (...args: Array<*>) => Promise<*>,
+                       f2: (*) => Promise<*>):
+                      (...args: Array<*>) => Promise<*> {
+  return async function composed (...args: Array<*>): Promise<*> {
+    return f2(await f1(...args))
+  }
+}
