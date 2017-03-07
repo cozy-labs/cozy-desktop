986bc77609b335f2c116886a771252cdefafcc9e
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-03-07 15:34:59 +0100

    utils/pending: Introduce PendingMap#hasParentPath()

--- test/unit/utils/pending.js
+++ test/unit/utils/pending.js
@@ -9,14 +9,33 @@ import type { Pending } from '../../../src/utils/pending' // eslint-disable-line
 
 describe('utils/pending/PendingMap', () => {
   let map
+  beforeEach(() => { map = new PendingMap() })
+
   const whatever: Pending = {
     execute: () => {},
     stopChecking: () => {}
   }
 
-  describe('hasPendingChild', function () {
-    beforeEach(() => { map = new PendingMap() })
+  describe('hasParentPath', function () {
+    it('is true when parent dir of the given path has some pending operation', () => {
+      should(map.hasParentPath('')).be.false()
+      should(map.hasParentPath('foo')).be.false()
+      should(map.hasParentPath('foo/bar')).be.false()
 
+      map.add('foo', whatever)
+      map.add('foo/bar', whatever)
+
+      should(map.hasParentPath('')).be.false()
+      should(map.hasParentPath('foo')).be.false()
+      should(map.hasParentPath('foo/missing/whatever')).be.false()
+      should(map.hasParentPath('foo/bar')).be.true()
+      should(map.hasParentPath('foo/missing')).be.true()
+      should(map.hasParentPath('foo/bar/missing')).be.true()
+      should(map.hasParentPath('missing')).be.false()
+    })
+  })
+
+  describe('hasPendingChild', function () {
     it('returns true if a sub-folder is pending', function () {
       map.add('bar', whatever)
       map.add('foo/bar', whatever)
