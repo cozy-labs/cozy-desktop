6c5075019860ac4a488157c3df0d2cf2e80b72cf
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-04-02 08:38:20 +0200

    side: Add async method definitions

--- src/side.js
+++ src/side.js
@@ -10,12 +10,21 @@ export type SideName =
 // eslint-disable-next-line no-undef
 export interface Side {
   addFile (doc: Metadata, callback: Callback): void;
+  addFileAsync (doc: Metadata): Promise<*>;
   addFolder (doc: Metadata, callback: Callback): void|Promise<*>;
+  addFolderAsync (doc: Metadata): Promise<*>;
   overwriteFile (doc: Metadata, old: Metadata, callback: Callback): void|Promise<*>;
+  overwriteFileAsync (doc: Metadata, old: Metadata): Promise<*>;
   updateFileMetadata (doc: Metadata, old: Metadata, callback: Callback): void;
+  updateFileMetadataAsync (doc: Metadata, old: Metadata): Promise<*>;
   updateFolder (doc: Metadata, old: Metadata, callback: Callback): void;
+  updateFolderAsync (doc: Metadata, old: Metadata): Promise<*>;
   moveFile (doc: Metadata, from: Metadata, callback: Callback): void;
+  moveFileAsync (doc: Metadata, from: Metadata): Promise<*>;
   moveFolder (doc: Metadata, from: Metadata, callback: Callback): void;
+  moveFolderAsync (doc: Metadata, from: Metadata): Promise<*>;
   trash (doc: Metadata, callback: Callback): void;
+  trashAsync (doc: Metadata): Promise<*>;
   destroy (doc: Metadata, callback: Callback): void;
+  destroyAsync (doc: Metadata): Promise<*>;
 }
