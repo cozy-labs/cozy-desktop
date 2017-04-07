3bf5ff4782c93cfdf599ceef2110033c00878d58
Sébastien Nicouleaud <sebastien@nicouleaud.net>
2017-04-07 08:18:07 +0200

    logger: Use bunyan

--- src/logger.js
+++ src/logger.js
@@ -1,197 +1,27 @@
 /* @flow weak */
 
-import prettyFormat from '@ava/pretty-format'
-import {
-  blue,
-  dim,
-  gray,
-  green,
-  magenta,
-  red,
-  yellow
-} from 'chalk'
-import * as diff from 'diff'
+import bunyan from 'bunyan'
+import os from 'os'
+import path from 'path'
 
-let printit = options => new Logger(options)
-if (printit.console == null) { printit.console = global.console }
+export const defaultLogger = bunyan.createLogger({
+  name: 'Cozy Desktop',
+  level: 'debug',
+  serializers: {
+    err: bunyan.stdSerializers.err
+  },
+  streams: [
+    {
+      type: 'rotating-file',
+      path: path.join(process.env.COZY_DESKTOP_DIR || os.homedir(), '.cozy-desktop', 'logs.txt'),
+      period: '1d',
+      count: 7
+    }
+  ]
+})
 
-const theme = {
-  boolean: 'dim',
-  content: 'dim',
-  date: 'dim',
-  function: 'dim',
-  label: 'dim',
-  misc: 'dim',
-  number: 'dim',
-  prop: 'dim',
-  value: 'dim',
-  regex: 'dim',
-  string: 'dim',
-  tag: 'dim',
-
-  key: 'italic',
-
-  bracket: 'reset',
-  comma: 'reset',
-  symbol: 'reset',
-
-  error: 'red'
+function logger (options) {
+  return defaultLogger.child(options)
 }
 
-function highlight (obj) {
-  return prettyFormat(obj, {highlight: true, theme, min: true})
-}
-
-class Logger {
-  options: Object
-
-  constructor (options) {
-    this.options = options
-    if (this.options == null) { this.options = {} }
-    if (this.options.date && (this.options.dateFormat == null)) {
-      this.options.dateFormat = 'YYYY-MM-DD hh:mm:ss:S'
-    }
-  }
-
-  colorify (text, color) {
-    return `${color[0]}${text}${color[1]}`
-  }
-
-  stringify (text) {
-    if (text instanceof Error && text.stack) {
-      text = text.stack
-    } else if (text instanceof Object) {
-      text = JSON.stringify(text)
-    }
-    return text
-  }
-
-  getFileAndLine () {
-    let stacklist = (new Error()).stack.split('\n').slice(4)
-    let nodeReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi
-    let browserReg = /at\s+()(.*):(\d*):(\d*)/gi
-
-    let firstLineStack = stacklist[0]
-    let fileAndLineInfos =
-            nodeReg.exec(firstLineStack) || browserReg.exec(firstLineStack)
-
-    let filePath = fileAndLineInfos[2].substr(process.cwd().length)
-    let line = fileAndLineInfos[3]
-    return `.${filePath}:${line} |`
-  }
-
-  format (level, texts) {
-    var text = ((() => {
-      let result = []
-      for (text of Array.from(texts)) {
-        if (typeof text === 'string') {
-          result.push(this.stringify(text))
-        } else if (text.stack) {
-          result.push(text.stack)
-        } else {
-          result.push(highlight(text))
-        }
-      }
-      return result
-    })()).join(' ')
-
-    if (level === 'success') { text = green(text) }
-    if (level === 'error') { text = red(text) }
-    if (level === 'debug') { text = dim(text) }
-
-    let prefix = this.options.prefix
-    if (prefix != null) {
-      if (prefix.startsWith('Local')) prefix = magenta(prefix)
-      if (prefix.startsWith('Remote')) prefix = blue(prefix)
-      if (prefix.startsWith('Pouch')) prefix = yellow(prefix)
-      if (level === 'debug') prefix = dim(prefix)
-
-      text = `${prefix} | ${text}`
-    }
-
-    if (this.options.date) {
-      const date = new Date()
-      let ms = date.getMilliseconds().toString()
-      ms = '000'.substring(0, 3 - ms.length) + ms
-      const timestamp = gray(`[${date.toLocaleString()} ${ms}]`)
-      text = `${timestamp} ${text}`
-    }
-    return text
-  }
-
-  info (...texts) {
-    if (process.env.DEBUG || (process.env.NODE_ENV !== 'test')) {
-      return printit.console.info(this.format('info', texts))
-    }
-  }
-
-  success (...texts) {
-    if (process.env.DEBUG || (process.env.NODE_ENV !== 'test')) {
-      return printit.console.info(this.format('success', texts))
-    }
-  }
-
-  warn (...texts) {
-    if (process.env.DEBUG || (process.env.NODE_ENV !== 'test')) {
-      if (this.options.duplicateToStdout) {
-        printit.console.info(this.format('warn', texts))
-      }
-      return printit.console.warn(this.format('warn', texts))
-    }
-  }
-
-  error (...texts) {
-    if (process.env.DEBUG || (process.env.NODE_ENV !== 'test')) {
-      if (this.options.duplicateToStdout) {
-        printit.console.info(this.format('error', texts))
-      }
-      return printit.console.error(this.format('error', texts))
-    }
-  }
-
-  errorIfAny (err) {
-    if (err) {
-      this.error(err)
-    }
-  }
-
-  debug (...texts) {
-    if (process.env.DEBUG) {
-      return printit.console.info(this.format('debug', texts))
-    }
-  }
-
-  inspect (obj) {
-    if (process.env.DEBUG) {
-      this.raw(highlight(obj))
-    }
-  }
-
-  diff (was, obj) {
-    if (process.env.DEBUG) {
-      const lines = diff.diffJson(was, obj)
-
-      this.raw(lines.map(line => {
-        if (line.added) {
-          return green(line.value)
-        } else if (line.removed) {
-          return red(line.value)
-        } else {
-          return ''
-        }
-      }).join(''))
-    }
-  }
-
-  raw (...texts) {
-    if (process.env.DEBUG) {
-      return printit.console.log(...texts)
-    }
-  }
-
-  lineBreak (text) {
-    return this.raw(Array(80).join('*'))
-  }
-}
-
-export default printit
+export default logger
