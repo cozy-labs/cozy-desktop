395228e7b9af1ea57f1a6f55184add69ff956b9e
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-05-26 12:01:38 +0200

    Introduce utils/fs.hideOnWindows()

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,3 +1,25 @@
+import logger from '../logger'
+
+const log = logger({
+  component: 'FS'
+})
+
+// The fswin module can only be loaded on Windows
+let fswin
+if (process.platform === 'win32') {
+  fswin = require('fswin')
+}
+
+// Hides a file or directory on Windows.
+// Async so it doesn't block more useful operations.
+// No failure handling because we can't / don't want to do anything anyway.
+export function hideOnWindows (path: string): void {
+  if (!fswin) return
+  fswin.setAttributes(path, {IS_HIDDEN: true}, (succeeded) => {
+    if (!succeeded) log.warn(`Could not set IS_HIDDEN flag on ${path}`)
+  })
+}
+
 const ILLEGAL_CHARACTERS = '/?<>\\:*|"'
 const ILLEGAL_CHARACTERS_REGEXP = new RegExp(`[${ILLEGAL_CHARACTERS}]`, 'g')
 const REPLACEMENT_CHARACTER = '_'
