2ba5f32c6ac887d70bc06d17cdfd19a95e114888
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-06-08 22:13:46 +0200

    Introduce trash integration test

---
+++ cli/test/integration/v3/trash.js
@@ -0,0 +1,55 @@
+import {
+  afterEach,
+  beforeEach,
+  suite,
+  test
+} from 'mocha'
+import pick from 'lodash.pick'
+import should from 'should'
+import sinon from 'sinon'
+
+import Ignore from '../../../src/ignore'
+import Merge from '../../../src/merge'
+import Prep from '../../../src/prep'
+
+import MetadataBuilders from '../../builders/metadata'
+import configHelpers from '../../helpers/config'
+import pouchHelpers from '../../helpers/pouch'
+
+suite('Trash', () => {
+  let builders, ignore, merge, pouch, prep
+
+  beforeEach(configHelpers.createConfig)
+  beforeEach(pouchHelpers.createDatabase)
+  afterEach(pouchHelpers.cleanDatabase)
+  afterEach(configHelpers.cleanConfig)
+
+  beforeEach(function () {
+    pouch = this.pouch
+    builders = new MetadataBuilders(pouch)
+    merge = new Merge(pouch)
+    ignore = new Ignore([])
+    prep = new Prep(merge, ignore)
+  })
+
+  test('local dir', async () => {
+    const dir = await builders.dirMetadata().path('parent/dir').create()
+    const child = await builders.dirMetadata().path('parent/dir/child').create()
+    sinon.spy(pouch, 'put')
+
+    const promise = prep.trashFolderAsync('local', {path: 'parent/dir'})
+
+    await should(promise).be.rejectedWith({status: 409, name: 'conflict'})
+    const putDocs = pouch.put.args.map(args => {
+      const doc = args[0]
+      return pick(doc, ['path', '_deleted', 'trashed'])
+    })
+    should(putDocs).deepEqual([
+      {path: 'parent/dir/child', _deleted: true},
+      {path: 'parent/dir', _deleted: true},
+      {path: 'parent/dir', trashed: true}
+    ])
+    await should(pouch.db.get(dir._id)).be.rejectedWith({status: 404})
+    await should(pouch.db.get(child._id)).be.rejectedWith({status: 404})
+  })
+})
