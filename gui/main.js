dd58959f2c39195c2abe13011afd19e0c1f055b9
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-07-21 10:52:02 +0200

    Pin synced folder to quick access on Windows 10+

--- gui/main.js
+++ gui/main.js
@@ -518,57 +518,9 @@ const execSync = (cmd) => {
   console.log(output)
 }
 
-/*
-// Retrieve the Windows SID for the current user
-const windowsCurrentUserSID = () => {
-  const {username} = os.userInfo()
-  const command = `wmic useraccount where name="${username}" get sid`
-  const output = childProcess.execSync(command, {encoding: 'utf8'})
-  return output.split(/\s+/)[1]
+const win10PinToHome = (path) => {
+  execSync(`powershell -Command "(New-Object -COM shell.application).Namespace('${path}').Self.InvokeVerb('pintohome')"`)
 }
-
-// Compute the Windows sync root id (See windowsRegisterCloudStorageProvider)
-const windowsSyncRootId = (windowsSID, accountID) => {
-  const storageProviderID = 'CozyDrive'
-  return `${storageProviderID}!${windowsSID}!${accountID}`
-}
-
-// See: https://msdn.microsoft.com/en-us/library/windows/desktop/dn889934.aspx
-const windowsRegisterCloudStorageProvider = (syncPath) => {
-  const windowsSID = windowsCurrentUserSID()
-  const syncRootId = windowsSyncRootId(windowsSID, 'default')
-  const clsid = uuid.v4().toUpperCase()
-  const exePath = process.argv[0]
-
-  execSync(`reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\SyncRootManager\\${syncRootId} /v DisplayNameResource /t REG_SZ /d "Cozy Drive" /f`)
-  execSync(`reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\SyncRootManager\\${syncRootId} /v IconResource /t REG_SZ /d "${exePath}" /f`)
-  execSync(`reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\SyncRootManager\\${syncRootId}\\UserSyncRoots /v ${windowsSID} /t REG_SZ /d "${syncPath}" /f`)
-
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}} /ve /t REG_SZ /d "Cozy Drive" /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\DefaultIcon /ve /t REG_SZ /d "${exePath}" /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}} /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0x1 /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}} /v SortOrderIndex /t REG_DWORD /d 0x42 /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\InProcServer32 /ve /t REG_EXPAND_SZ /d %%systemroot%%\\system32\\shell32.dll /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\Instance /v CLSID /t REG_SZ /d {0E5AAE11-A475-4c5b-AB00-C66DE400274E} /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\Instance\\InitPropertyBag /v Attributes /t REG_DWORD /d 0x11 /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\Instance\\InitPropertyBag /v TargetFolderPath /t REG_EXPAND_SZ /d "${syncPath}" /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\ShellFolder /v FolderValueFlags /t REG_DWORD /d 0x28 /f`)
-  execSync(`reg add HKCU\\Software\\Classes\\CLSID\\{${clsid}}\\ShellFolder /v Attributes /t REG_DWORD /d 0xF080004D /f`)
-  execSync(`reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${clsid}} /ve /t REG_SZ /d "Cozy Drive" /f`)
-  execSync(`reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\HideDesktopIcons\\NewStartPanel /v {${clsid}} /t REG_DWORD /d 0x1 /f`)
-
-  return {syncRootId, clsid}
-}
-
-const windowsUnregisterCloudStorageProvider = (windowsConfig) => {
-  const {syncRootId, clsid} = windowsConfig
-
-  execSync(`reg delete HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\SyncRootManager\\${syncRootId} /f`)
-  execSync(`reg delete HKCU\\Software\\Classes\\CLSID\\{${clsid}} /f`)
-  execSync(`reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${clsid}} /f`)
-  execSync(`reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\HideDesktopIcons\\NewStartPanel /v {${clsid}} /f`)
-}
-*/
 
 const sfltoolAddFavorite = (path) => {
   const item = url.resolve('file://', path)
@@ -581,36 +533,14 @@ const major = Number.parseInt(os.release().split('.')[0])
 // https://en.wikipedia.org/wiki/Darwin_(operating_system)#Release_history
 
 const addFileManagerShortcut = (config) => {
-  /*
   if (platform === 'win32' && major >= 10) {
-    const windowsConfig = windowsRegisterCloudStorageProvider(config.syncPath)
-    config.save('windows', windowsConfig).then(() => config.persist())
-  } else
-  */
-  if (platform === 'darwin' && major >= 15) {
+    win10PinToHome(config.syncPath)
+  } else if (platform === 'darwin' && major >= 15) {
     // sfltool is available since 10.11 (El Capitan)
     sfltoolAddFavorite(config.syncPath)
   } else {
     console.log(`Not registering shortcut on ${platform} ${major}`)
   }
-}
-
-const removeFileManagerShortcut = (config) => {
-  /*
-  if (platform === 'win32' && major >= 10) {
-    const windowsConfig = config.config.windows
-    if (windowsConfig) {
-      windowsUnregisterCloudStorageProvider(windowsConfig)
-    } else {
-      console.log('Not unregistering shortcut because windows config is missing')
-    }
-  } else {
-  */
-  console.log(`Not unregistering shortcut on ${platform} ${major}`)
-  /*
-  }
-  */
-  // FIXME: Not removing favorite on macOS >= 10.11 since sfltool does not support it.
 }
 
 app.on('ready', () => {
@@ -731,7 +661,6 @@ ipcMain.on('unlink-cozy', () => {
       return
     }
     desktop.stopSync().then(() => {
-      removeFileManagerShortcut(desktop.config)
       desktop.removeRemote()
         .then(() => console.log('removed'))
         .then(() => sendToMainWindow('unlinked'))
