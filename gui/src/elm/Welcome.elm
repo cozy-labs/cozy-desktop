74d303c9f3d215123cd1501121d104e207c5f5e9
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-08-01 16:38:14 +0200

    update styles to match new guidelines

--- gui/src/elm/Welcome.elm
+++ gui/src/elm/Welcome.elm
@@ -3,7 +3,10 @@ module Welcome exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Icons exposing (..)
 import Helpers exposing (Helpers)
+import Svg exposing (svg, node, path)
+import Svg.Attributes exposing (fill, d, viewBox)
 
 
 -- UPDATE
@@ -16,23 +19,29 @@ type Msg
 
 -- VIEW
 
-
-view : Helpers -> Html Msg
-view helpers =
+view : Helpers -> String -> Html Msg
+view helpers platform =
     div
         [ classList
             [ ( "step", True )
             , ( "step-welcome", True )
             ]
         ]
-        [ div [ class "upper" ] []
-        , div [ class "upper" ]
-            [ img [ src "images/happycloud.png" ] [] ]
-        , h1 [] [ text (helpers.t "Welcome Your own private cloud") ]
-        , a
-            [ class "btn"
-            , href "#"
-            , onClick NextPage
-            ]
-            [ text (helpers.t "Welcome Sign in to your Cozy") ]
+        [ div
+          [ class "step-content" ]
+          [ Icons.cozyBigIcon
+          , h1 [] [ text (helpers.t "Welcome Welcome Cozy Desktop") ]
+          , h1 [] [ text (helpers.t "Welcome Your own private cloud") ]
+          , a
+              [ class "btn"
+              , href "#"
+              , onClick NextPage
+              ]
+              [ text (helpers.t "Welcome Sign in to your Cozy") ]
+          , a
+              [ href ("https://cozy.io/en/try-it/?from=desktop-" ++ platform)
+              , class "more-info"
+              ]
+              [ text (helpers.t "Address Don't have an account? Request one here") ]
+          ]
         ]
