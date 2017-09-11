9e8d02f471ff5bf308e4bfb4da494a05c3dfc0c2
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-09-11 17:30:35 +0200

    Introduce LocalEventBuffer

---
+++ cli/src/local/event_buffer.js
@@ -0,0 +1,66 @@
+/* @flow */
+
+type EventBufferMode = 'idle' | 'timeout'
+type FlushCallback<EventType> = (EventType[]) => void
+
+// An event buffer.
+// Needs a flush callback to be called everytime the buffer is flushed.
+//
+// In *idle* mode (default), the buffer will only store pushed events and
+// you'll have to flush it manually.
+// In *timeout* mode, the buffer flushes stored events by itself when nothing
+// happens during `timeoutInMs`.
+//
+// The buffer will also flush automatically when switching from one mode to
+// another.
+//
+// Right now this class is in the local/ namespace because this is where it is
+// used, but it could be anywhere else since it doesn't have any dependency.
+export default class EventBuffer<EventType> {
+  events: EventType[]
+  mode: EventBufferMode
+  timeoutInMs: number
+  timeout: *
+  flushed: FlushCallback<EventType>
+
+  constructor (timeoutInMs: number, flushed: FlushCallback<EventType>) {
+    this.events = []
+    this.mode = 'idle'
+    this.timeoutInMs = timeoutInMs
+    this.timeout = null
+    this.flushed = flushed
+  }
+
+  push (event: EventType): void {
+    this.events.push(event)
+    this.shiftTimeout()
+  }
+
+  shiftTimeout (): void {
+    if (this.mode === 'timeout') {
+      this.clearTimeout()
+      this.timeout = setTimeout(this.flush, this.timeoutInMs)
+    }
+  }
+
+  clearTimeout (): void {
+    if (this.timeout != null) {
+      clearTimeout(this.timeout)
+      delete this.timeout
+    }
+  }
+
+  flush (): void {
+    this.clearTimeout()
+    if (this.events.length > 0) {
+      const flushedEvents = this.events
+      this.events = []
+      this.flushed(flushedEvents)
+    }
+  }
+
+  switchMode (mode: EventBufferMode): void {
+    this.flush()
+    this.mode = mode
+  }
+}
