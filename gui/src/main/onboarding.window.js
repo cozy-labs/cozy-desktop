4750573c29509a26741886f30ad50db2f5c6f5f6
Romain <romain@cozycloud.cc>
2017-09-26 14:31:32 +0200

    Factor code between windows

--- gui/src/main/onboarding.window.js
+++ gui/src/main/onboarding.window.js
@@ -1,5 +1,5 @@
 const {addFileManagerShortcut} = require('./shortcut')
-const {BrowserWindow, dialog, ipcMain, session} = require('electron')
+const {dialog, session} = require('electron')
 const autoLaunch = require('./autolaunch')
 const {translate} = require('./i18n')
 
@@ -14,122 +14,97 @@ const LOGIN_SCREEN_HEIGHT = 700
 const OAUTH_SCREEN_WIDTH = ONBOARDING_SCREEN_WIDTH
 const OAUTH_SCREEN_HEIGHT = 930
 
-const behaviours = require('./window_behaviour')
+const WindowManager = require('./window_behaviour')
 
-let afterOnboarding = null
-
-let win = null
-let app = null
-let desktop = null
-
-module.exports.init = (appRef, desktopRef) => {
-  desktop = desktopRef
-  app = appRef
-}
-
-module.exports.show = () => {
-  if (!win) module.exports.create()
-  win.show()
-}
-
-module.exports.hide = () => {
-  if (win) win.close()
-  win = null
-}
-
-module.exports.onOnboardingDone = (next) => {
-  afterOnboarding = next
-}
-
-module.exports.create = () => {
-  log.debug({'buildonboarding': true})
-  win = new BrowserWindow({
-    title: 'ONBOARDING',
-    center: true,
-    'auto-hide-menu-bar': true,
-    icon: `${__dirname}/images/icon.png`,
-    width: ONBOARDING_SCREEN_WIDTH,
-    height: ONBOARDING_SCREEN_HEIGHT
-  })
-  win.loadURL(`file://${__dirname}/../../index.html`)
-  // win.setMenu(Menu.buildFromTemplate([]))
-  behaviours.noMenu(win)
-  behaviours.devTools(win)
-  behaviours.dockApple(app, win)
-  behaviours.openExternalLinks(win)
-  win.on('closed', () => { win = null })
-  // win.webContents.on('dom-ready', appLoaded)
-}
-
-module.exports.send = (...args) => {
-  log.debug({'there': win && !!win.webContents})
-  if (win && win.webContents) {
-    win.webContents.send(...args)
+module.exports = class OnboardingWM extends WindowManager {
+  windowOptions () {
+    return {
+      title: 'ONBOARDING',
+      center: true,
+      'auto-hide-menu-bar': true,
+      icon: `${__dirname}/images/icon.png`,
+      width: ONBOARDING_SCREEN_WIDTH,
+      height: ONBOARDING_SCREEN_HEIGHT
+    }
   }
-}
 
-ipcMain.on('register-remote', (event, arg) => {
-  const cozyUrl = desktop.checkCozyUrl(arg.cozyUrl)
-  desktop.config.cozyUrl = cozyUrl
-  const onRegistered = (client, url) => {
-    let resolveP
-    const promise = new Promise((resolve) => { resolveP = resolve })
-    win.setContentSize(LOGIN_SCREEN_WIDTH, LOGIN_SCREEN_HEIGHT, true)
-    win.loadURL(url)
-    win.webContents.on('did-get-response-details', (event, status, newUrl, originalUrl, httpResponseCode) => {
-      if (newUrl.match(/\/auth\/authorize\?/) && httpResponseCode === 200) {
-        const bounds = win.getBounds()
-        const display = electron.screen.getDisplayMatching(bounds)
-        const height = Math.min(display.workAreaSize.height - bounds.y, OAUTH_SCREEN_HEIGHT)
-        win.setSize(OAUTH_SCREEN_WIDTH, height, true)
-      }
+  ipcEvents () {
+    return {
+      'register-remote': this.onRegisterRemote,
+      'choose-folder': this.onChooseFolder,
+      'start-sync': this.onStartSync
+    }
+  }
+
+  onOnboardingDone (handler) {
+    this.afterOnboarding = handler
+  }
+
+  onRegisterRemote (event, arg) {
+    let desktop = this.desktop()
+    const cozyUrl = desktop.checkCozyUrl(arg.cozyUrl)
+    desktop.config.cozyUrl = cozyUrl
+    const onRegistered = (client, url) => {
+      let resolveP
+      const promise = new Promise((resolve) => { resolveP = resolve })
+      this.win.setContentSize(LOGIN_SCREEN_WIDTH, LOGIN_SCREEN_HEIGHT, true)
+      this.win.loadURL(url)
+      this.win.webContents.on('did-get-response-details', (event, status, newUrl, originalUrl, httpResponseCode) => {
+        if (newUrl.match(/\/auth\/authorize\?/) && httpResponseCode === 200) {
+          const bounds = win.getBounds()
+          const display = electron.screen.getDisplayMatching(bounds)
+          const height = Math.min(display.workAreaSize.height - bounds.y, OAUTH_SCREEN_HEIGHT)
+          win.setSize(OAUTH_SCREEN_WIDTH, height, true)
+        }
+      })
+      this.win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+        if (newUrl.match('file://')) {
+          this.win.setContentSize(ONBOARDING_SCREEN_WIDTH, ONBOARDING_SCREEN_HEIGHT, true)
+          log.logger({'did-get-red': newUrl})
+          resolveP(newUrl)
+        }
+      })
+      return promise
+    }
+    desktop.registerRemote(cozyUrl, arg.location, onRegistered)
+      .then(
+        (reg) => {
+          session.defaultSession.clearStorageData()
+          this.win.webContents.once('dom-ready', () => setTimeout(() => event.sender.send('registration-done'), 20))
+          this.win.loadURL(reg.client.redirectURI)
+          autoLaunch.setEnabled(true)
+        },
+        (err) => {
+          log.error(err)
+          event.sender.send('registration-error', translate('Address No cozy instance at this address!'))
+        }
+      )
+  }
+
+  onChooseFolder (event) {
+    let folders = dialog.showOpenDialog({
+      properties: ['openDirectory', 'createDirectory']
     })
-    win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
-      if (newUrl.match('file://')) {
-        win.setContentSize(ONBOARDING_SCREEN_WIDTH, ONBOARDING_SCREEN_HEIGHT, true)
-        log.logger({'did-get-red': newUrl})
-        resolveP(newUrl)
-      }
-    })
-    return promise
+    if (folders && folders.length > 0) {
+      event.sender.send('folder-chosen', folders[0])
+    }
   }
-  desktop.registerRemote(cozyUrl, arg.location, onRegistered)
-    .then(
-      (reg) => {
-        session.defaultSession.clearStorageData()
-        win.webContents.once('dom-ready', () => setTimeout(() => event.sender.send('registration-done'), 20))
-        win.loadURL(reg.client.redirectURI)
-        autoLaunch.setEnabled(true)
-      },
-      (err) => {
-        log.error(err)
-        event.sender.send('registration-error', translate('Address No cozy instance at this address!'))
-      }
-    )
-})
 
-ipcMain.on('choose-folder', (event) => {
-  let folders = dialog.showOpenDialog({
-    properties: ['openDirectory', 'createDirectory']
-  })
-  if (folders && folders.length > 0) {
-    event.sender.send('folder-chosen', folders[0])
-  }
-})
-
-ipcMain.on('start-sync', (event, syncPath) => {
-  if (!desktop.config.isValid()) {
-    log.error('No client!')
-    return
-  }
-  try {
-    desktop.saveConfig(desktop.config.cozyUrl, syncPath)
+  onStartSync (event, syncPath) {
+    let desktop = this.desktop()
+    if (!desktop.config.isValid()) {
+      log.error('No client!')
+      return
+    }
     try {
-      addFileManagerShortcut(desktop.config)
-    } catch (err) { log.error(err) }
-    afterOnboarding()
-  } catch (err) {
-    log.error(err)
-    event.sender.send('folder-error', translate('Error Invalid path'))
+      desktop.saveConfig(desktop.config.cozyUrl, syncPath)
+      try {
+        addFileManagerShortcut(desktop.config)
+      } catch (err) { log.error(err) }
+      this.afterOnboarding()
+    } catch (err) {
+      log.error(err)
+      event.sender.send('folder-error', translate('Error Invalid path'))
+    }
   }
-})
+}
--- gui/src/main/tray.window.js
+++ gui/src/main/tray.window.js
@@ -1,4 +1,4 @@
-const {BrowserWindow, dialog, shell, ipcMain} = require('electron')
+const {dialog, shell} = require('electron')
 const {spawn} = require('child_process')
 const autoUpdater = require('./autoupdate')
 const autoLaunch = require('./autolaunch')
@@ -12,114 +12,101 @@ const log = require('cozy-desktop').default.logger({
   component: 'GUI'
 })
 
-const behaviours = require('./window_behaviour')
+const WindowManager = require('./window_behaviour')
 
-let win = null
-let app = null
-let desktop = null
-
-module.exports.init = (appRef, desktopRef) => {
-  desktop = desktopRef
-  app = appRef
-}
-
-module.exports.create = () => {
-  log.debug({'create': 'onboarding'})
-  if (win != null) return
-  win = new BrowserWindow({
-    title: 'TRAY',
-    icon: `${__dirname}/images/icon.png`,
-    windowPosition: (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter',
-    frame: false,
-    show: false,
-    skipTaskbar: true,
-    // transparent: true,
-    width: DASHBOARD_SCREEN_WIDTH,
-    height: DASHBOARD_SCREEN_HEIGHT
-  })
-  win.loadURL(`file://${__dirname}/../../index.html#dashboard`)
-  behaviours.noMenu(win)
-  // behaviours.devTools(win)
-  behaviours.dockApple(app, win)
-  behaviours.openExternalLinks(win)
-  win.on('closed', () => { win = null })
-  win.on('blur', () => setTimeout(() => { if (!win.isFocused() && !win.isDevToolsFocused()) win.close() }, 400))
-  win.positioner = new Positioner(win)
-}
-
-module.exports.onReady = (cb) => {
-  win && win.webContents && win.webContents.once('dom-ready', () => {
-    setTimeout(cb, 100) // elm initialization
-  })
-}
-
-module.exports.send = (...args) => {
-  if (win && win.webContents) {
-    win.webContents.send(...args)
-  }
-}
-
-module.exports.reload = () => {
-  if (win) win.reload()
-}
-
-module.exports.show = (trayPos) => {
-  if (!win) module.exports.create()
-  let pos = null
-  if (trayPos === undefined || trayPos.x === 0) {
-    pos = (process.platform === 'win32') ? 'bottomRight' : 'topRight'
-  } else {
-    pos = (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter'
+module.exports = class TrayWM extends WindowManager {
+  windowOptions () {
+    return {
+      title: 'TRAY',
+      icon: `${__dirname}/images/icon.png`,
+      windowPosition: (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter',
+      frame: false,
+      show: false,
+      skipTaskbar: true,
+      // transparent: true,
+      width: DASHBOARD_SCREEN_WIDTH,
+      height: DASHBOARD_SCREEN_HEIGHT
+    }
   }
 
-  win.positioner.move(pos, trayPos)
-  win.show()
-}
-
-module.exports.hide = () => {
-  if (win) win.close()
-  win = null
-}
-
-ipcMain.on('go-to-cozy', () => shell.openExternal(desktop.config.cozyUrl))
-ipcMain.on('go-to-folder', () => shell.openItem(desktop.config.syncPath))
-ipcMain.on('quit-and-install', () => autoUpdater.quitAndInstall())
-ipcMain.on('auto-launcher', (event, enabled) => autoLaunch.setEnabled(enabled))
-ipcMain.on('logout', () => {
-  desktop.removeConfig()
-  win.send('unlinked')
-})
-
-ipcMain.on('unlink-cozy', () => {
-  if (!desktop.config.isValid()) {
-    log.error('No client!')
-    return
+  create () {
+    log.debug({tcreate: true})
+    let pReady = super.create()
+    log.debug({tcreateafer: true})
+    this.positioner = new Positioner(this.win)
+    this.win.on('blur', this.onBlur.bind(this))
+    log.debug({tcreateafer2: true})
+    return pReady
   }
-  const options = {
-    type: 'question',
-    title: translate('Unlink Title'),
-    message: translate('Unlink Message'),
-    detail: translate('Unlink Detail'),
-    buttons: [translate('Unlink Cancel'), translate('Unlink OK')],
-    cancelId: 0,
-    defaultId: 1
+
+  show (trayPos) {
+    let pReady = (this.win) ? Promise.resolve(this.win) : this.create()
+
+    let pos = null
+
+    if (trayPos === undefined || trayPos.x === 0) {
+      pos = (process.platform === 'win32') ? 'bottomRight' : 'topRight'
+    } else {
+      pos = (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter'
+    }
+    this.positioner.move(pos, trayPos)
+    this.win.show()
+
+    return pReady
   }
-  dialog.showMessageBox(win, options, (response) => {
-    if (response === 0) {
-      win.send('cancel-unlink')
+
+  onBlur () {
+    setTimeout(() => {
+      if (!this.win.isFocused() && !this.win.isDevToolsFocused()) this.win.close()
+    }, 400)
+  }
+
+  ipcEvents () {
+    return {
+      'go-to-cozy': () => shell.openExternal(this.desktop.config.cozyUrl),
+      'go-to-folder': () => shell.openItem(this.desktop.config.syncPath),
+      'quit-and-install': () => autoUpdater.quitAndInstall(),
+      'auto-launcher': (event, enabled) => autoLaunch.setEnabled(enabled),
+      'logout': () => {
+        this.desktop.removeConfig()
+        this.win.send('unlinked')
+      },
+      'unlink-cozy': this.onUnlink,
+      'restart': this.onRestart
+    }
+  }
+
+  onUnlink () {
+    if (!this.desktop.config.isValid()) {
+      log.error('No client!')
       return
     }
-    desktop.stopSync().then(() => {
-      desktop.removeRemote()
-        .then(() => log.info('removed'))
-        .then(() => win.send('unlinked'))
-        .catch((err) => log.error(err))
+    const options = {
+      type: 'question',
+      title: translate('Unlink Title'),
+      message: translate('Unlink Message'),
+      detail: translate('Unlink Detail'),
+      buttons: [translate('Unlink Cancel'), translate('Unlink OK')],
+      cancelId: 0,
+      defaultId: 1
+    }
+    dialog.showMessageBox(this.win, options, (response) => {
+      if (response === 0) {
+        this.win.send('cancel-unlink')
+        return
+      }
+      this.desktop.stopSync().then(() => {
+        this.desktop.removeRemote()
+          .then(() => log.info('removed'))
+          .then(() => this.win.send('unlinked'))
+          .catch((err) => log.error(err))
+      })
     })
-  })
-})
+  }
 
-ipcMain.on('restart', () => {
-  setTimeout(app.quit, 50)
-  const args = process.argv.slice(1).filter(a => a !== '--isHidden')
-  spawn(process.argv[0], args, { detached: true })
-})
+  onRestart () {
+    setTimeout(this.app.quit, 50)
+    const args = process.argv.slice(1).filter(a => a !== '--isHidden')
+    spawn(process.argv[0], args, { detached: true })
+  }
+}
