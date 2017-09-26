74ff3764f0392ab6bc19456088f38e20c95b5895
Romain <romain@cozycloud.cc>
2017-09-26 14:31:32 +0200

    Factor code between windows

--- gui/src/main/help.window.js
+++ gui/src/main/help.window.js
@@ -1,47 +1,36 @@
-const {BrowserWindow, ipcMain} = require('electron')
-const behaviours = require('./window_behaviour')
+const WindowManager = require('./window_behaviour')
 const HELP_SCREEN_WIDTH = 768
 const HELP_SCREEN_HEIGHT = 570
 
-let win = null
-let app = null
-let desktop = null
-
-module.exports.init = (appRef, desktopRef) => {
-  desktop = desktopRef
-  app = appRef
-}
-
-module.exports.create = () => {
-  if (win != null) return
-  win = new BrowserWindow({
-    title: 'HELP',
-    icon: `${__dirname}/images/icon.png`,
-    width: HELP_SCREEN_WIDTH,
-    height: HELP_SCREEN_HEIGHT
-  })
-  win.loadURL(`file://${__dirname}/../../index.html#help`)
-  behaviours.noMenu(win)
-  behaviours.devTools(win)
-  behaviours.dockApple(app, win)
-  behaviours.openExternalLinks(win)
-  win.on('closed', () => { win = null })
-}
-
-ipcMain.on('send-mail', (event, body) => {
-  desktop.sendMailToSupport(body).then(
-    () => { event.sender.send('mail-sent') },
-    (err) => {
-      event.sender.send('mail-sent', {
-        message: err.message,
-        name: err.name,
-        stack: err.stack
-      })
+module.exports = class TrayWM extends WindowManager {
+  windowOptions () {
+    return {
+      title: 'HELP',
+      icon: `${__dirname}/images/icon.png`,
+      width: HELP_SCREEN_WIDTH,
+      height: HELP_SCREEN_HEIGHT
     }
-  )
-})
+  }
 
-module.exports.show = () => {
-  if (!win) module.exports.create()
-  win.show()
+  hash () {
+    return '#help'
+  }
+
+  ipcEvents () {
+    let that = this
+    return {
+      'send-mail': (event, body) => {
+        that.desktop.sendMailToSupport(body).then(
+          () => { event.sender.send('mail-sent') },
+          (err) => {
+            event.sender.send('mail-sent', {
+              message: err.message,
+              name: err.name,
+              stack: err.stack
+            })
+          }
+        )
+      }
+    }
+  }
 }
--- gui/src/main/window_behaviour.js
+++ gui/src/main/window_behaviour.js
@@ -1,29 +1,103 @@
-const {shell} = require('electron')
+const {BrowserWindow, ipcMain, shell} = require('electron')
 
-module.exports.openExternalLinks = (win) => {
-  win.webContents.on('will-navigate', (event, url) => {
-    if (url.startsWith('http') && !url.match('/auth/authorize')) {
-      event.preventDefault()
-      shell.openExternal(url)
-    }
-  })
-  return win
-}
+const ELMSTARTUP = 400
 
-module.exports.noMenu = (win) => {
-  win.setMenu(null)
-  win.setAutoHideMenuBar(true)
-}
+const log = require('cozy-desktop').default.logger({
+  component: 'windows'
+})
 
-module.exports.devTools = (win) => {
-  if (process.env.WATCH === 'true' || process.env.DEBUG === 'true') {
-    win.webContents.openDevTools({mode: 'detach'})
+module.exports = class WindowManager {
+  constructor (app, desktop) {
+    this.win = null
+    this.app = app
+    this.desktop = desktop
+    this.log = require('cozy-desktop').default.logger({
+      component: 'window' + this.windowOptions().title
+    })
+
+    let handlers = this.ipcEvents()
+    Object.keys(handlers).forEach((name) => {
+      if (!handlers[name]) {
+        throw new Error('undefined handler for event ' + name)
+      }
+      ipcMain.on(name, handlers[name])
+    })
   }
-}
 
-module.exports.dockApple = (app, win) => {
-  if (process.platform === 'darwin') {
-    app.dock.show()
-    win.on('closed', () => { app.dock.hide() })
+  /* abtract */
+  windowOptions () {
+    throw new Error('extends WindowManager before use')
+  }
+
+  /* abtract */
+  ipcEvents () {
+    throw new Error('extends WindowManager before use')
+  }
+
+  show () {
+    if (!this.win) return this.create()
+    this.win.show()
+    return Promise.resolve(this.win)
+  }
+
+  hide () {
+    if (this.win) this.win.close()
+    this.win = null
+  }
+
+  reload () {
+    if (this.win) this.win.reload()
+  }
+
+  send (...args) {
+    this.win && this.win.webContents && this.win.webContents.send(...args)
+  }
+
+  hash () {
+    return ""
+  }
+
+  create () {
+    this.win = new BrowserWindow(this.windowOptions())
+
+    // openExternalLinks
+    this.win.webContents.on('will-navigate', (event, url) => {
+      if (url.startsWith('http') && !url.match('/auth/authorize')) {
+        event.preventDefault()
+        shell.openExternal(url)
+      }
+    })
+
+    // noMenu
+    this.win.setMenu(null)
+    this.win.setAutoHideMenuBar(true)
+
+    // dockApple
+    if (process.platform === 'darwin') {
+      this.app.dock.show()
+      this.win.on('closed', () => { this.app.dock.hide() })
+    }
+
+    // dont keep  hidden windows objects
+    this.win.on('closed', () => { this.win = null })
+
+    let resolveRef = null
+    let pReady = new Promise((resolve, reject) => {
+      resolveRef = resolve
+    }).catch((err) => log.error(err))
+
+    this.win.webContents.on('dom-ready', () => {
+      setTimeout(() => { resolveRef(this.win) }, ELMSTARTUP)
+    })
+
+    this.win.loadURL(`file://${__dirname}/../../index.html${this.hash()}`)
+
+
+    // devTools
+    if (process.env.WATCH === 'true' || process.env.DEBUG === 'true') {
+      this.win.webContents.openDevTools({mode: 'detach'})
+    }
+
+    return pReady
   }
 }
