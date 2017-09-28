803590236e46c76206da5da2badd9a8742b2dfb5
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-09-28 16:22:37 +0200

    Ensure fixture helpers don't depend on babel

--- cli/test/fixtures/local_watcher/index.js
+++ cli/test/fixtures/local_watcher/index.js
@@ -1,19 +1,19 @@
-import fs from 'fs-extra'
-import path from 'path'
+const Promise = require('bluebird')
+const fs = require('fs-extra')
+const path = require('path')
 
-export const scenarios =
+module.exports.scenarios =
   fs.readdirSync(__dirname)
     .filter(name => name.endsWith('.scenario.js'))
     .map(name => {
       const scenarioPath = path.join(__dirname, name)
-      return {
-        ...require(scenarioPath),
-        name,
-        path: scenarioPath
-      }
+      const scenario = require(scenarioPath)
+      scenario.name = name
+      scenario.path = scenarioPath
+      return scenario
     })
 
-export const loadFSEvents = (scenario, platform) => {
+module.exports.loadFSEvents = (scenario, platform) => {
   const eventsFile = scenario.path.replace(/\.scenario\.js$/, `.fsevents.${platform}.json`)
   return fs.readJson(eventsFile)
 }
