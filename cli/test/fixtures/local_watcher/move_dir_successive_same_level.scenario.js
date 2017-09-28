93f449c49c74117ee28c276562e45a6f3ec859bf
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-09-28 15:44:15 +0200

    Fix successive move fixture expectations

--- cli/test/fixtures/local_watcher/move_dir_successive_same_level.scenario.js
+++ cli/test/fixtures/local_watcher/move_dir_successive_same_level.scenario.js
@@ -17,7 +17,11 @@ module.exports = {
     prepCalls: [
       { method: 'putFolderAsync', path: 'parent/dst2/dir' },
       { method: 'putFolderAsync', path: 'parent/dst2/dir/empty-subdir' },
-      { method: 'putFolderAsync', path: 'parent/dst2/dir/subdir' }
+      { method: 'putFolderAsync', path: 'parent/dst2/dir/subdir' },
+      {method: 'moveFileAsync', dst: 'parent/dst2/dir/subdir/file', src: 'parent/src/dir/subdir/file'},
+      {method: 'trashFolderAsync', path: 'parent/src/dir/subdir'},
+      {method: 'trashFolderAsync', path: 'parent/src/dir/empty-subdir'},
+      {method: 'trashFolderAsync', path: 'parent/src/dir'},
     ],
   },
 }
