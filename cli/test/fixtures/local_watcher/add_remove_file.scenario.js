757f1edc6b7675cfe9853d4d84dba9238f3ec9ae
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-10-03 09:22:07 +0200

    Introduce LocalWatcher automated fixture-based tests
    So we can:
    - Write up a local change scenario
    - Capture chokidar events for each platform
    - Automatically use those as fixtures in a test suite

---
+++ cli/test/fixtures/local_watcher/add_remove_file.scenario.js
@@ -0,0 +1,11 @@
+module.exports = {
+  actions: [
+    {type: '>', path: 'file'},
+    {type: 'wait', ms: 1500},
+    {type: 'rm', path: 'file'}
+  ],
+  expected: {
+    prepCalls: [
+    ]
+  }
+}
