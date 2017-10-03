12fca81ceabe74623c66a9458163b8f28956c936
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-10-03 09:24:53 +0200

    Introduce ContextualizedChokidarFSEvent types

--- cli/src/local/chokidar_event.js
+++ cli/src/local/chokidar_event.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import fs from 'fs'
+import type { Metadata } from '../metadata'
 
 type ChokidarAdd = {type: 'add', path: string, stats: fs.Stats}
 type ChokidarAddDir = {type: 'addDir', path: string, stats: fs.Stats}
@@ -21,3 +22,16 @@ export const build = (type: string, path?: string, stats?: fs.Stats): ChokidarFS
   if (stats != null) event.stats = stats
   return event
 }
+
+type ContextualizedChokidarAdd = ChokidarAdd & {md5sum: string, sameChecksums: Metadata[]}
+type ContextualizedChokidarAddDir = ChokidarAddDir & {ino: string}
+type ContextualizedChokidarChange = ChokidarChange & {md5sum: string}
+type ContextualizedChokidarUnlink = ChokidarUnlink
+type ContextualizedChokidarUnlinkDir = ChokidarUnlinkDir
+
+export type ContextualizedChokidarFSEvent =
+  | ContextualizedChokidarAdd
+  | ContextualizedChokidarAddDir
+  | ContextualizedChokidarChange
+  | ContextualizedChokidarUnlink
+  | ContextualizedChokidarUnlinkDir
