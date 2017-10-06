63997f6865fe7d7c9319e624521f743e4b4001db
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-10-06 09:22:40 +0200

    LocalWatcher fixtures: FIXME: Add scenarios + events (FIXME: macOS only)
    Includes scenarios that we don't handle yet

---
+++ cli/test/fixtures/local_watcher/add_remove_dir.scenario.js
@@ -0,0 +1,11 @@
+module.exports = {
+  actions: [
+    {type: 'mkdir', path: 'dir'},
+    {type: 'wait', ms: 1500},
+    {type: 'rm', path: 'dir'}
+  ],
+  expected: {
+    prepCalls: [
+    ]
+  }
+}
