5528ef76a647648ab20aa2e83bf9d6b70fc994a3
Romain <romain@cozycloud.cc>
2017-10-09 17:44:03 +0200

    lint

--- cli/test/fixtures/local_watcher/move_file_successive/scenario.js
+++ cli/test/fixtures/local_watcher/move_file_successive/scenario.js
@@ -3,12 +3,12 @@ module.exports = {
     {ino: 1, path: 'dst1/'},
     {ino: 2, path: 'dst2/'},
     {ino: 3, path: 'src/'},
-    {ino: 4, path: 'src/file'},
+    {ino: 4, path: 'src/file'}
   ],
   actions: [
     {type: 'mv', src: 'src/file', dst: 'dst1/file'},
     {type: 'wait', ms: 1500},
-    {type: 'mv', src: 'dst1/file', dst: 'dst2/file'},
+    {type: 'mv', src: 'dst1/file', dst: 'dst2/file'}
   ],
   expected: {
     prepCalls: [
