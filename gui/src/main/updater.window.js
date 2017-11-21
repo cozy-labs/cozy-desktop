77445c4966520831769cb7c5faad326216980209
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-11-21 14:32:46 +0100

    gui: Update check timeout

--- gui/src/main/updater.window.js
+++ gui/src/main/updater.window.js
@@ -5,6 +5,8 @@ const log = require('cozy-desktop').default.logger({
   component: 'GUI:autoupdater'
 })
 
+const UPDATE_CHECK_TIMEOUT = 5000
+
 module.exports = class UpdaterWM extends WindowManager {
   windowOptions () {
     return {
@@ -16,8 +18,10 @@ module.exports = class UpdaterWM extends WindowManager {
 
   constructor (...opts) {
     autoUpdater.on('update-available', (info) => {
-      log.info({update: info}, 'Update available')
-      this.send('update-downloading', null)
+      this.clearTimeoutIfAny()
+      log.info({update: info, skipped: this.skipped}, 'Update available')
+      // Make sure UI don't show up in front of onboarding after timeout
+      if (!this.skipped) this.show()
     })
     autoUpdater.on('update-not-available', (info) => {
       log.info({update: info}, 'No update available')
@@ -25,6 +29,7 @@ module.exports = class UpdaterWM extends WindowManager {
     })
     autoUpdater.on('error', (err) => {
       log.error({err}, 'Error in auto-updater! ')
+      // May happen in dev because of code signature error. Not really an issue.
       this.afterUpToDate()
     })
     autoUpdater.on('download-progress', (progressObj) => {
@@ -39,16 +44,39 @@ module.exports = class UpdaterWM extends WindowManager {
     super(...opts)
   }
 
-  onUpToDate (handler) {
-    this.afterUpToDate = handler
+  clearTimeoutIfAny () {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+      this.timeout = null
+    }
   }
 
-  show (...opts) {
-    let pShown = super.show(...opts)
-    log.info('Looking for updates...')
-    if (process.platform === 'linux') this.afterUpToDate()
-    else autoUpdater.checkForUpdates()
-    return pShown
+  onUpToDate (handler) {
+    this.afterUpToDate = () => {
+      this.clearTimeoutIfAny()
+      handler()
+    }
+  }
+
+  checkForUpdates () {
+    if (process.platform === 'linux') {
+      log.warn(`Not looking for updates on ${process.platform}.`)
+      this.afterUpToDate()
+      return Promise.resolve()
+    } else {
+      log.info('Looking for updates...')
+      this.timeout = setTimeout(() => {
+        log.warn({timeout: UPDATE_CHECK_TIMEOUT}, 'Updates check is taking too long')
+        this.skipped = true
+
+        // Disable handler & warn on future calls
+        const handler = this.afterUpToDate
+        this.afterUpToDate = () => {}
+
+        handler()
+      }, UPDATE_CHECK_TIMEOUT)
+      autoUpdater.checkForUpdates()
+    }
   }
 
   hash () {
