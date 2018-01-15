313849d5e4f8af90132359482f053ee3500725f5
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-01-15 09:54:18 +0100

    core/local/chokidar_event: Contextualized* are intersection types

--- core/local/chokidar_event.js
+++ core/local/chokidar_event.js
@@ -23,11 +23,11 @@ export const build = (type: string, path?: string, stats?: fs.Stats): ChokidarFS
   return event
 }
 
-type ContextualizedChokidarAdd = {type: 'add', path: string, stats: fs.Stats, md5sum: string, wip?: true}
-type ContextualizedChokidarAddDir = {type: 'addDir', path: string, stats: fs.Stats, wip?: true}
-type ContextualizedChokidarChange = {type: 'change', path: string, stats: fs.Stats, md5sum: string, wip?: true}
-type ContextualizedChokidarUnlink = {type: 'unlink', old: ?Metadata, path: string}
-type ContextualizedChokidarUnlinkDir = {type: 'unlinkDir', old: ?Metadata, path: string}
+type ContextualizedChokidarAdd = ChokidarAdd & {md5sum: string, wip?: true}
+type ContextualizedChokidarAddDir = ChokidarAddDir & {wip?: true}
+type ContextualizedChokidarChange = ChokidarChange & {md5sum: string, wip?: true}
+type ContextualizedChokidarUnlink = ChokidarUnlink & {old: ?Metadata}
+type ContextualizedChokidarUnlinkDir = ChokidarUnlinkDir & {old: ?Metadata}
 
 export type ContextualizedChokidarFSEvent =
   | ContextualizedChokidarAdd
