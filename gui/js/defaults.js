68600e317903e2a2e0ed8511dd1a02e939694778
sebn <sebn@users.noreply.github.com>
2018-02-13 11:16:35 +0100

    Improve development environment (#978)
    * Add sample env file
    * Suggest `tmp/Cozy Drive` as default synced dir

--- gui/js/defaults.js
+++ gui/js/defaults.js
@@ -3,4 +3,4 @@
 const os = require('os')
 const path = require('path')
 
-module.exports.syncPath = path.join(os.homedir(), 'Cozy Drive')
+module.exports.syncPath = path.resolve(process.env.COZY_DESKTOP_DIR || os.homedir(), 'Cozy Drive')
