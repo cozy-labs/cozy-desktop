c40cf8e85a01f09462e0692f1af5fafbfe9d3716
Romain <romain@cozycloud.cc>
2018-02-20 11:06:36 +0100

    core/upload: add Content-Length header

--- core/file_stream_provider.js
+++ core/file_stream_provider.js
@@ -4,8 +4,16 @@ import * as stream from 'stream'
 
 import type { Metadata } from './metadata'
 
+export class ReadableWithContentLength extends stream.Readable {contentLength: ?number}
+
+export function withContentLength (s: stream.Readable, contentLength: ?number): ReadableWithContentLength {
+  const s2: ReadableWithContentLength = (s: any)
+  s2.contentLength = contentLength
+  return s2
+}
+
 // Provides a stream.Readable for local or remote file corresponding to the
 // given metadata.
 export type FileStreamProvider = {
-  createReadStreamAsync: (Metadata) => Promise<stream.Readable>;
+  createReadStreamAsync: (Metadata) => Promise<ReadableWithContentLength>;
 }
