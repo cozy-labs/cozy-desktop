0ccdc0ccf55fbb7ff6575c3bbc0fab3ad694a2f4
Romain <romain@cozycloud.cc>
2018-03-13 14:07:18 +0100

    test/scenarios/move_and_update_file: Introduce new scenario

---
+++ test/scenarios/move_and_update_file/scenario.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import type { Scenario } from '..'
+
+module.exports = ({
+  init: [
+    {ino: 1, path: 'dst/'},
+    {ino: 2, path: 'src/'},
+    {ino: 3, path: 'src/file'}
+  ],
+  actions: [
+    {type: 'mv', src: 'src/file', dst: 'dst/file'},
+    {type: 'wait', ms: 1500},
+    {type: '>>', path: 'dst/file'}
+  ],
+  expected: {
+    prepCalls: [
+      {method: 'moveFileAsync', src: 'src/file', dst: 'dst/file'},
+      {method: 'updateFileAsync', path: 'dst/file'}
+    ],
+    tree: [
+      'dst/',
+      'dst/file',
+      'src/'
+    ]
+  }
+}: Scenario)
