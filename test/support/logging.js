ed0c3030875aea7317c6f4b4a7b3e4e9bedc5b93
sebn <sebn@users.noreply.github.com>
2018-04-26 09:43:34 +0200

    test/support/logging: Dump failing test logs (#1048)

--- test/support/logging.js
+++ test/support/logging.js
@@ -3,11 +3,31 @@
 
 import logger from '../../core/logger'
 
+const { defaultLogger } = logger
 const log = logger({component: 'mocha'})
 
+const lines = []
+
+defaultLogger.addStream({
+  type: 'raw',
+  level: 'trace',
+  stream: {
+    write: function (msg) {
+      lines.push(msg)
+    }
+  }
+})
+
 beforeEach(function () {
+  lines.length = 0
   // FIXME: this.currentTest is undefined on AppVeyor, not sure why
   if (process.env.APPVEYOR == null) {
     log.info('\n\n---------- ' + this.currentTest.title + ' ----------\n\n')
+  }
+})
+
+afterEach(function () {
+  if (this.currentTest.state === 'failed') {
+    console.log(lines.map(l => JSON.stringify(l)).join('\n'))
   }
 })
