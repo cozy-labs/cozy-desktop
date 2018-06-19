d7e2fa4556257d83285e8064a53d340565fa792d
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-06-19 09:03:28 +0200

    test/integration: Update test suite with Invalid Hash + conflict example

---
+++ test/integration/update.js
@@ -0,0 +1,93 @@
+/* @flow */
+/* eslint-env mocha */
+
+const _ = require('lodash')
+const should = require('should')
+
+const logger = require('../../core/logger')
+
+const { IntegrationTestHelpers } = require('../support/helpers/integration')
+const configHelpers = require('../support/helpers/config')
+const cozyHelpers = require('../support/helpers/cozy')
+const pouchHelpers = require('../support/helpers/pouch')
+
+const log = logger({component: 'mocha'})
+
+describe('Update file', () => {
+  let cozy, helpers, pouch, prep
+
+  before(configHelpers.createConfig)
+  before(configHelpers.registerClient)
+  beforeEach(pouchHelpers.createDatabase)
+  beforeEach(cozyHelpers.deleteAll)
+
+  afterEach(pouchHelpers.cleanDatabase)
+  after(configHelpers.cleanConfig)
+
+  beforeEach(async function () {
+    cozy = cozyHelpers.cozy
+    helpers = new IntegrationTestHelpers(this.config, this.pouch, cozy)
+    pouch = helpers._pouch
+    prep = helpers.prep
+
+    await helpers.local.setupTrash()
+    await helpers.remote.ignorePreviousChanges()
+  })
+
+  describe('M1, local merge M1, M2, remote sync M1, local merge M2', () => {
+    it('fails remote sync M1 & local merge M2', async () => {
+      const file = await cozy.files.create('Initial content', {name: 'file'})
+      await helpers.remote.pullChanges()
+      await helpers.syncAll()
+
+      log.info('-------- M1 --------')
+      const m1 = 'M1'
+      await helpers.local.syncDir.outputFile('file', m1)
+
+      log.info('-------- local merge M1 --------')
+      should(await helpers.local.syncDir.checksum('file')).equal('8x4e7yD2RzOhjFOAc+eDlg==')
+      await prep.updateFileAsync('local', _.defaults(
+        {
+          md5sum: await helpers.local.syncDir.checksum('file'),
+          size: 2
+        },
+        await pouch.byRemoteIdMaybeAsync(file._id)
+      ))
+
+      log.info('-------- M2 --------')
+      const m2 = 'M2'
+      await helpers.local.syncDir.outputFile('file', m2)
+
+      log.info('-------- remote sync M1 --------')
+      await helpers.syncAll()
+
+      log.info('-------- local merge M2 --------')
+      should(await helpers.local.syncDir.checksum('file')).equal('nYMiUwtn4jZuWxumcIHe2Q==')
+      await prep.updateFileAsync('local', _.defaults(
+        {
+          md5sum: await helpers.local.syncDir.checksum('file'),
+          size: 2
+        },
+        await pouch.byRemoteIdMaybeAsync(file._id)
+      ))
+
+      log.info('-------- remote sync M2 --------')
+      await helpers.syncAll()
+
+      should({
+        localTree: await helpers.local.tree(),
+        remoteTree: await helpers.remote.tree(),
+        remoteFileContent: await helpers.remote.readFile('file')
+      }).deepEqual({
+        localTree: [
+          'file'
+        ],
+        remoteTree: [
+          '.cozy_trash/',
+          'file'
+        ],
+        remoteFileContent: m2
+      })
+    })
+  })
+})
