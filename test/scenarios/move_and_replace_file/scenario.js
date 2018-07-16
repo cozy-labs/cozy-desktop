759b938e80d315a6c9b53e6325c10a033bab1fe0
Romain <romain@cozycloud.cc>
2018-07-16 11:29:37 +0200

    Move & replac etest

---
+++ test/scenarios/move_and_replace_file/scenario.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+/*:: import type { Scenario } from '..' */
+
+module.exports = ({
+  init: [
+    { ino: 1, path: 'dst/' },
+    { ino: 2, path: 'dst/file' },
+    { ino: 3, path: 'src/' },
+    { ino: 4, path: 'src/file' }
+  ],
+  actions: [
+    {type: 'trash', path: 'dst/file'},
+    {type: 'mv', src: 'src/file', dst: 'dst/file'}
+  ],
+  expected: {
+    prepCalls: [
+      {method: 'trashFileAsync', path: 'dst/file'},
+      {method: 'moveFileAsync', dst: 'dst/file', src: 'src/file'}
+    ],
+    tree: [
+      'dst/',
+      'dst/file',
+      'src/'
+    ],
+    remoteTrash: [
+      'file'
+    ]
+  }
+} /*: Scenario */)
