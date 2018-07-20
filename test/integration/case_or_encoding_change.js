be946c4f73e55982a755160ab1a6efc0a14a72fa
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-07-20 10:02:48 +0200

    test: Explain why encoding test is disabled on Travis macOS

--- test/integration/case_or_encoding_change.js
+++ test/integration/case_or_encoding_change.js
@@ -16,6 +16,12 @@ const pouchHelpers = require('../support/helpers/pouch')
 const { IntegrationTestHelpers } = require('../support/helpers/integration')
 
 suite('Case or encoding change', () => {
+  // This test passes on a macOS workstation when using the docker container,
+  // since files are stored on an ext4 FS. But Travis macOS workers lack
+  // virtualization support, which means the cozy-stack will run directly on
+  // macOS with HFS+ and the test will fail (see below).
+  // Maybe we could create an ext4 volume on Travis and put the storage files
+  // there to better match the production environment?
   if (process.env.TRAVIS && (process.platform === 'darwin')) {
     test.skip('is unstable on Travis (macOS)')
     return
@@ -44,6 +50,8 @@ suite('Case or encoding change', () => {
     let dir, dir2
 
     beforeEach(async () => {
+      // This will fail with a 409 conflict error when cozy-stack runs directly
+      // on macOS & HFS+ because a file with an equivalent name already exists.
       dir = await cozy.files.createDirectory({name: '\u0065\u0301'})
       dir2 = await cozy.files.createDirectory({name: 'foo'})
       await helpers.remote.pullChanges()
