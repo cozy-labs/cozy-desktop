d6f843602e6013cb240cd6e5025e1fe403d84564
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-07-27 12:45:00 +0200

    test/support/assertions: await should(dir).have.fileContents({...})
    So we don't test files one at a time.
    So when first file content doesn't match, we don't end up wondering
    whether second file content does or not.

---
+++ test/support/assertions/fileContents.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+const _ = require('lodash')
+const should = require('should')
+
+/*::
+import type { ContextDir } from '../helpers/context_dir'
+*/
+
+// Usage:
+//
+//     const dir = new ContextDir(...)
+//     await should(dir).have.fileContents({
+//       file1: 'content 1',
+//       file2: 'content 2'
+//     })
+should.Assertion.prototype.fileContents = async function (expected /*: { [path: string]: string } */) {
+  const dir /*: ContextDir */ = this.obj
+  const actual /*: { [path: string]: string } */ = {}
+
+  for (let relpath of _.keys(expected)) {
+    try {
+      actual[relpath] = await dir.readFile(relpath)
+    } catch (err) {
+      actual[relpath] = `<Error: ${err.message}>`
+    }
+  }
+
+  should(actual).deepEqual(expected)
+}
