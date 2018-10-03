72d0b41ede12417823c82ea3de0c71292430a458
Romain Foucault <aenario@gmail.com>
2018-10-03 11:15:29 +0200

    core/logger: Add support for sending invariants to Sentry (#1177)

---
+++ test/unit/sentry.js
@@ -0,0 +1,29 @@
+/* eslint-env mocha */
+
+require('should')
+
+const sentry = require('../../core/sentry')
+
+describe('Sentry', function () {
+  describe('toSentryContext', function () {
+    it('properly parse all urls', function () {
+      sentry.toSentryContext('https://somedevcozy.cozy.tools:8080').should.deepEqual({
+        domain: 'cozy.tools',
+        instance: 'somedevcozy.cozy.tools',
+        environment: 'development'
+      })
+
+      sentry.toSentryContext('https://example.mycozy.cloud').should.deepEqual({
+        domain: 'mycozy.cloud',
+        instance: 'example.mycozy.cloud',
+        environment: 'production'
+      })
+
+      sentry.toSentryContext('https://mycozy-example.com').should.deepEqual({
+        domain: 'mycozy-example.com',
+        instance: 'mycozy-example.com',
+        environment: 'selfhost'
+      })
+    })
+  })
+})
