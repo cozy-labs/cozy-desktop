2879edb916cc627ffd168be80a4de93364e46b8f
Erwan Guyader <erwan.guyader@cozycloud.cc>
2018-10-29 14:27:02 +0100

    travis: Make built artifacts available in each PR
      Each time we build a Pull Request on travis, we build the binary
      artifacts for linux and mac OSX.
    
      We upload those binaries to bintray.com and add a comment on the PR so
      we can easily find them and test the Pull Request before merging it.

--- dev/ci/dist.sh
+++ dev/ci/dist.sh
@@ -19,3 +19,18 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 else
   yarn dist:all
 fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    version=$(git rev-parse --short HEAD);
+    bintray_version="PR-$TRAVIS_PULL_REQUEST";
+    dist_files=$(ls -p dist/ | grep -v /);
+
+    IFS=$'\n'
+    for file in $dist_files; do
+      bintray_path="$version-${file/ /%20}";
+      curl -u "$BINTRAY_USER:$BINTRAY_API_TOKEN" \
+           -T "dist/$file" \
+           "https://api.bintray.com/content/$BINTRAY_ORG/$BINTRAY_REPO/$BINTRAY_PACKAGE/$bintray_version/$bintray_path?publish=1&override=1";
+    done
+    unset IFS
+fi
