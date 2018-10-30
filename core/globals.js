90ad2c3af61cf8ddbea73fb00894fc2a96744df1
sebn <sebn@users.noreply.github.com>
2018-10-30 19:48:31 +0100

    core: DRY electron hack (#1232)

--- core/globals.js
+++ core/globals.js
@@ -1,4 +1,12 @@
+// FIXME: Use bluebird promises as long as we need asCallback
 const Promise = require('bluebird')
-
 global.Promise = Promise
 Promise.longStackTraces()
+
+// Network requests can be stuck with Electron on Linux inside the event loop.
+// A hack to deblock them is push some events in the event loop.
+// See https://github.com/electron/electron/issues/7083#issuecomment-262038387
+// And https://github.com/electron/electron/issues/1833
+if (process.platform === 'linux' && !process.env.NO_ELECTRON) {
+  setInterval(() => {}, 1000)
+}
