4eca958b55b62e462e0d0516610618e6691d8cea
Bruno Michel <bmichel@menfin.info>
2018-10-31 11:37:56 +0100

    Preserve order of batches in checksum layer

--- core/local/layers/checksum.js
+++ core/local/layers/checksum.js
@@ -8,30 +8,52 @@ import type { Layer } from './events'
 module.exports = class ChecksumLayer {
   /*::
   next: Layer
+  task : Promise<*>
   checksumer: Checksumer
   */
 
   constructor (next /*: Layer */, checksumer /*: Checksumer */) {
     this.next = next
+    this.task = Promise.resolve()
     this.checksumer = checksumer
   }
 
-  initial () {
-    // TODO wait that the batches of events are finished
-    return this.next.initial()
+  async initial () {
+    let result
+    const task = this.task
+    this.task = new Promise(async (resolve) => {
+      await task
+      result = this.next.initial()
+      resolve()
+    })
+    await this.task
+    return result
   }
 
   async process (events /*: Array<*> */) {
-    // TODO we should not start processing a batch of events before the previous one has finished
+    let result
+    const task = this.task
+    this.task = new Promise(async (resolve) => {
+      await task
+      events = await this.doProcess(events)
+      result = this.next.process(events)
+      resolve()
+    })
+    await this.task
+    return result
+  }
+
+  async doProcess (events /*: Array<*> */) {
     for (const event of events) {
       if (event.docType === 'file' && ['add', 'update'].includes(event.action)) {
         try {
           event.doc.checksum = await this.checksumer.push(event.doc.path)
         } catch (err) {
           // TODO
+          console.error(err)
         }
       }
     }
-    return this.next.process(events)
+    return events
   }
 }
