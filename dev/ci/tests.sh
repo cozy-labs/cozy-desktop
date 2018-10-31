b0786ab5b5d7461dd315a9e1617b65eada73f076
Bruno Michel <bmichel@menfin.info>
2018-10-31 18:15:16 +0100

    Run unit tests only one time on Travis/Linux

--- dev/ci/tests.sh
+++ dev/ci/tests.sh
@@ -8,7 +8,12 @@ yarn install:electron
 yarn install:elm
 yarn build:elm
 yarn lint
-yarn test:unit
-yarn test:world --timeout $MOCHA_TIMEOUT --forbid-only
 yarn test:elm
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	yarn test:unit:coverage --timeout $MOCHA_TIMEOUT --forbid-only
+	bash <(curl -s https://codecov.io/bash)
+else
+	yarn test:unit --timeout $MOCHA_TIMEOUT --forbid-only
+fi
+yarn test:world --timeout $MOCHA_TIMEOUT --forbid-only
 yarn test:integration --timeout $MOCHA_TIMEOUT --forbid-only
