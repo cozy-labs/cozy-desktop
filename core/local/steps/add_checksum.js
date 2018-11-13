44148a98474b7a66a7ee137ac42b6696af7773d2
Bruno Michel <bmichel@menfin.info>
2018-11-13 20:18:29 +0100

    Implements the checksum observable

--- core/local/steps/add_checksum.js
+++ core/local/steps/add_checksum.js
@@ -4,13 +4,29 @@ const { Observable } = require('rxjs')
 const { concatMap } = require('rxjs/operators')
 const Promise = require('bluebird')
 
-module.exports = function (observable /*: Observable<*> */) /*: Observable<*> */ {
+/*::
+import type { Checksumer } from '../checksumer'
+*/
+
+module.exports = function (observable /*: Observable<*> */, checksumer /*: Checksumer */) /*: Observable<*> */ {
   return observable.pipe(
-    concatMap((batch) => {
-      console.log('checksum', batch)
+    concatMap((events) => {
+      console.log('checksum', events)
       return Observable.create(async (observer) => {
-        // TODO compute checksum for events that need it
-        await Promise.delay(10)
+        const batch = []
+        for (const event of events) {
+          try {
+            if (['add', 'update'].includes(event.action) && event.docType === 'file') {
+              event.md5sum = await this.checksumer.push(event.abspath)
+            }
+            batch.push(event)
+          } catch (err) {
+            // TODO Currently, we ignore events when there is an error for
+            // computing the checksum as it is often just because the file has been
+            // deleted since. But we should have a more fine-grained error handling
+            // here.
+          }
+        }
         observer.next(batch)
         observer.complete()
       })
