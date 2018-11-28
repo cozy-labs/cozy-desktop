ac3d2f5f00b6e770b8ab8a53c9d07b8c429eaa4d
Enguerran <engcolson@gmail.com>
2018-11-28 12:45:54 +0100

    refactor: Tests use the locale.config

--- test/unit/gui/i18n.js
+++ test/unit/gui/i18n.js
@@ -2,34 +2,33 @@
 
 const should = require('should')
 const { selectLocale } = require('../../../gui/js/i18n')
+const {enabled, defaultLocale} = require('../../../gui/locales/locales.config.json')
 
 describe('Select locale for app', () => {
-  describe('Activated locales', () => {
-    it('should select fr as it is activated', () => {
-      const targetLocale = selectLocale('fr')
-      should(targetLocale).equal('fr')
+  describe('Enabled locales', () => {
+    enabled.forEach(enabledLocale => {
+      it(`should select ${enabledLocale} as it is enabled`, () => {
+        should(selectLocale(enabledLocale)).equal(enabledLocale)
+      })
     })
-    it('should select es as it is activated', () => {
-      const targetLocale = selectLocale('es')
-      should(targetLocale).equal('es')
-    })
-    it('should select en if given locale is not activated', () => {
-      const targetLocale = selectLocale('nl')
-      should(targetLocale).equal('en')
+    it('should select en if given locale is not enabled', () => {
+      const targetLocale = 'xx'
+      should(enabled.includes(targetLocale)).be.false()
+      should(selectLocale(targetLocale)).equal(defaultLocale)
     })
   })
   describe('Reduce to global locale instead of country locale', () => {
-    it('should be fr for fr_FR', () => {
-      const targetLocale = selectLocale('fr_FR')
-      should(targetLocale).equal('fr')
-    })
-    it('should be es for es_SP', () => {
-      const targetLocale = selectLocale('es_SP')
-      should(targetLocale).equal('es')
+    enabled.forEach(enabledLocale => {
+      const localizedLocale = `${enabledLocale}_FR`
+      it(`should select ${enabledLocale} for ${localizedLocale}`, () => {
+        should(selectLocale(localizedLocale)).equal(enabledLocale)
+      })
     })
     it('should be en for non activated locale', () => {
-      const targetLocale = selectLocale('nl_NL')
-      should(targetLocale).equal('en')
+      const targetLocale = 'xx'
+      const localizedLocale = `${targetLocale}_FR`
+      should(enabled.includes(targetLocale)).be.false()
+      should(selectLocale(localizedLocale)).equal('en')
     })
   })
 })
