92703d7c8c8ab42b12f383fdaf96980be49bf028
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-11-30 16:39:54 +0100

    test/scenarios: Reproduce subdir move after parent move issue

---
+++ test/scenarios/rename_subdir_after_parent/scenario.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+/*:: import type { Scenario } from '..' */
+
+module.exports = ({
+  side: 'local',
+  init: [
+    {ino: 1, path: 'parent/'},
+    {ino: 2, path: 'parent/subdir/'},
+    {ino: 3, path: 'parent/subdir/subsubdir/'}
+  ],
+  actions: [
+    {type: 'mv', src: 'parent', dst: 'parent-2'},
+    {type: 'wait', ms: 1500},
+    {type: 'mv', src: 'parent-2/subdir', dst: 'parent-2/subdir-2'},
+    {type: 'wait', ms: 1500},
+    {type: 'mv', src: 'parent-2/subdir-2/subsubdir', dst: 'parent-2/subdir-2/subsubdir-2'}
+  ],
+  expected: {
+    tree: [
+      'parent-2/',
+      'parent-2/subdir-2/',
+      'parent-2/subdir-2/subsubdir-2/'
+    ],
+    remoteTrash: []
+  }
+} /*: Scenario */)
