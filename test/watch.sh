cd7da0814ccd89bde5bbcd115246fbfcfd8b0583
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-12-21 08:45:08 +0100

    yarn: Add mocha:watch script
    Poor man's replacement for mocha --watch since electron-mocha doesn't
    support it.

---
+++ test/watch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Unlike mocha, electron-mocha lacks a --watch option.
+# This shell script uses chokidar as a poor man's replacement.
+# The `yarn mocha:watch` script makes it easier to discover.
+
+./node_modules/.bin/chokidar \
+  './core/**/*.js' \
+  './gui/js/**/*.js' \
+  './test/support/**/*.js' \
+  './test/mocha.opts' \
+  $@ \
+  -c "yarn mocha $@"
