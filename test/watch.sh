680ce0dcab7f3c73761971b0bd387478f7c70a26
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-12-21 08:54:40 +0100

    yarn: Run tests on first mocha:watch run

--- test/watch.sh
+++ test/watch.sh
@@ -4,10 +4,13 @@
 # This shell script uses chokidar as a poor man's replacement.
 # The `yarn mocha:watch` script makes it easier to discover.
 
+test_cmd="yarn mocha $@"
+
+$test_cmd
 ./node_modules/.bin/chokidar \
   './core/**/*.js' \
   './gui/js/**/*.js' \
   './test/support/**/*.js' \
   './test/mocha.opts' \
   $@ \
-  -c "yarn mocha $@"
+  -c "$test_cmd"
