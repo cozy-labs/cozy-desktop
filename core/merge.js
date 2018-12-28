a6af4661ae56b38831c54bb64337a632465c7dfa
Bruno Michel <bmichel@menfin.info>
2018-12-28 16:59:24 +0100

    Fix side number for file/folder created at a path used in the past

--- core/merge.js
+++ core/merge.js
@@ -144,6 +144,16 @@ class Merge {
     }
     if (doc.tags == null) { doc.tags = [] }
     await this.ensureParentExistAsync(side, doc)
+    const info = await this.pouch.put(doc)
+    // If a file has existed as this ID and was deleted, the pouchdb doc will
+    // have a revision higher than the expected 1-xxx, and we should update the
+    // side in this case
+    let rev = metadata.extractRevNumber({ _rev: info.rev })
+    if (rev === 1) {
+      return info
+    }
+    doc.sides[side] = ++rev
+    doc._rev = info.rev
     return this.pouch.put(doc)
   }
 
@@ -221,6 +231,16 @@ class Merge {
     }
     if (doc.tags == null) { doc.tags = [] }
     await this.ensureParentExistAsync(side, doc)
+    const info = await this.pouch.put(doc)
+    // If a folder has existed as this ID and was deleted, the pouchdb doc will
+    // have a revision higher than the expected 1-xxx, and we should update the
+    // side in this case
+    let rev = metadata.extractRevNumber({ _rev: info.rev })
+    if (rev === 1) {
+      return info
+    }
+    doc.sides[side] = ++rev
+    doc._rev = info.rev
     return this.pouch.put(doc)
   }
 
