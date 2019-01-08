cf570116732fa2776891458cbe565b484e65f8a8
Erwan Guyader <erwan@guyader.fr>
2019-01-08 16:02:27 +0100

    Revert "Enable atom/watcher by default for Windows and Linux"

--- dev/ci/scenarios.sh
+++ dev/ci/scenarios.sh
@@ -4,9 +4,5 @@ set -ex
 
 . "./dev/ci/start_xvfb.sh"
 
-# FIXME Scenario tests use ChokidarEvents and onFlush
-# So, they are not yet compatible with atom/watcher
-export COZY_FS_WATCHER=chokidar
-
 yarn install:electron
 yarn test:scenarios --timeout $MOCHA_TIMEOUT --forbid-only
