21800a546552182fff94334efa0fb938d7b9a11f
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-01-09 17:35:47 +0100

    core/local/steps/initial_diff: Add Flow types

--- core/local/steps/initial_diff.js
+++ core/local/steps/initial_diff.js
@@ -10,15 +10,20 @@ const log = logger({
 
 /*::
 import type Pouch from '../../pouch'
+import type { Batch, EventKind } from './event'
+import type { Metadata } from '../../metadata'
+
+type WatchedPath = {
+  path: string,
+  kind: EventKind,
+}
 */
 
-// TODO add unit tests
-
 // Some files and directories can have been deleted while cozy-desktop was
-// stopped. So, at the end of the initial scan, we have to to a diff between
+// stopped. So, at the end of the initial scan, we have to do a diff between
 // what was in pouchdb and the events from the local watcher to find what was
 // deleted.
-async function initialDiff (buffer, out, pouch) {
+async function initialDiff (buffer /*: Buffer */, out /*: Buffer */, pouch /*: Pouch */) /*: Promise<void> */ {
   // XXX we wait to receive the first batch of events before initializing this
   // component, as pouchdb may not be initialized when initialDiff is created
   // (its views are added later, but before the local watcher is started, thus
@@ -28,15 +33,14 @@ async function initialDiff (buffer, out, pouch) {
   // Using inode/fileId is more robust that using path or id for detecting
   // which files/folders have been deleted, as it is stable even if the
   // file/folder has been moved or renamed
-  const byInode = new Map()
-  const docs = await pouch.byRecursivePathAsync('')
+  const byInode /*: Map<number|string, WatchedPath> */ = new Map()
+  const docs /*: Metadata[] */ = await pouch.byRecursivePathAsync('')
   for (const doc of docs) {
-    if (!doc.ino) {
-      // Ignore files/dirs created on the remote and never synchronized
-      continue
+    if (doc.ino != null) {
+      // Process only files/dirs that were created locally or synchronized
+      const kind = doc.docType === 'file' ? 'file' : 'directory'
+      byInode.set(doc.fileid || doc.ino, { path: doc.path, kind: kind })
     }
-    const kind = doc.docType === 'file' ? 'file' : 'directory'
-    byInode.set(doc.fileid || doc.ino, { path: doc.path, kind: kind })
   }
   let done = false
 
@@ -46,7 +50,7 @@ async function initialDiff (buffer, out, pouch) {
       continue
     }
 
-    const batch = []
+    const batch /*: Batch */ = []
     for (const event of events) {
       if (event.incomplete) {
         batch.push(event)
@@ -55,7 +59,7 @@ async function initialDiff (buffer, out, pouch) {
 
       // Detect if the file was moved while the client was stopped
       if (['created', 'scan'].includes(event.action)) {
-        let was
+        let was /*: ?WatchedPath */
         if (event.stats.fileid) {
           was = byInode.get(event.stats.fileid)
         }
