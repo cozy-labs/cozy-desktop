0a58a45e7b740f4c2d590f882a9636b4bfcff0d0
Enguerran <engcolson@gmail.com>
2019-01-09 09:45:59 +0100

    fix: Run linux_producer tests on linux platform only

--- test/unit/local/steps/linux_producer.js
+++ test/unit/local/steps/linux_producer.js
@@ -7,166 +7,174 @@ const fs = require('fs')
 const path = require('path')
 const LinuxProducer = require('../../../../core/local/steps/linux_producer')
 
-describe.only('core/local/steps/linux_producer', () => {
-  describe('API of the producer', () => {
-    let syncPath
-    let producer
+if (os.platform() === 'linux') {
+  describe('core/local/steps/linux_producer', () => {
+    describe('API of the producer', () => {
+      let syncPath
+      let producer
 
-    beforeEach(() => {
-      syncPath = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'))
-      producer = new LinuxProducer({
-        syncPath
+      beforeEach(() => {
+        syncPath = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'))
+        producer = new LinuxProducer({
+          syncPath
+        })
+      })
+
+      it('should register on start and dispose on stop the watcher', async () => {
+        await producer.start()
+        should(producer.watcher).be.not.null()
+        await producer.stop()
+        should(producer.watcher).be.null()
+      })
+
+      it('should change absolute path to relative with process', async () => {
+        const dirname = 'foobar'
+        const batch = [
+          {
+            action: 'created',
+            kind: 'directory',
+            path: path.join(syncPath, dirname)
+          }
+        ]
+        producer.process(batch.map(evt => Object.assign({}, evt)))
+        should(await producer.buffer.pop()).eql(
+          batch.map(evt => Object.assign({}, evt, { path: dirname }))
+        )
+      })
+
+      it('should scan a subfolder tree', async () => {
+        const mypath = ['foo', 'bar', 'baz']
+        mkdirSyncRecursive(syncPath, mypath.join('/'))
+        await producer.start()
+        should(
+          (await producer.buffer.pop()).map(({ action, kind, path }) => ({
+            action,
+            kind,
+            path
+          }))
+        ).eql([
+          {
+            action: 'scan',
+            kind: 'unknown',
+            path: mypath[0]
+          }
+        ])
+        should(
+          (await producer.buffer.pop()).map(({ action, kind, path }) => ({
+            action,
+            kind,
+            path
+          }))
+        ).eql([
+          {
+            action: 'scan',
+            kind: 'unknown',
+            path: `${mypath[0]}/${mypath[1]}`
+          }
+        ])
+        should(
+          (await producer.buffer.pop()).map(({ action, kind, path }) => ({
+            action,
+            kind,
+            path
+          }))
+        ).eql([
+          {
+            action: 'scan',
+            kind: 'unknown',
+            path: mypath.join('/')
+          }
+        ])
       })
     })
 
-    it('should register on start and dispose on stop the watcher', async () => {
-      await producer.start()
-      should(producer.watcher).be.not.null()
-      await producer.stop()
-      should(producer.watcher).be.null()
-    })
+    describe('register an event on FS events', () => {
+      let syncPath
+      let producer
 
-    it('should change absolute path to relative with process', async () => {
-      const dirname = 'foobar'
-      const batch = [
-        {
-          action: 'created',
-          kind: 'directory',
-          path: path.join(syncPath, dirname)
-        }
-      ]
-      producer.process(batch.map(evt => Object.assign({}, evt)))
-      should(await producer.buffer.pop()).eql(
-        batch.map(evt => Object.assign({}, evt, { path: dirname }))
-      )
-    })
-
-    it('should scan a subfolder tree', async () => {
-      const mypath = ['foo', 'bar', 'baz']
-      mkdirSyncRecursive(syncPath, mypath.join('/'))
-      await producer.start()
-      should(
-        (await producer.buffer.pop()).map(({ action, kind, path }) => ({
-          action,
-          kind,
-          path
-        }))
-      ).eql([
-        {
-          action: 'scan',
-          kind: 'unknown',
-          path: mypath[0]
-        }
-      ])
-      should(
-        (await producer.buffer.pop()).map(({ action, kind, path }) => ({
-          action,
-          kind,
-          path
-        }))
-      ).eql([
-        {
-          action: 'scan',
-          kind: 'unknown',
-          path: `${mypath[0]}/${mypath[1]}`
-        }
-      ])
-      should(
-        (await producer.buffer.pop()).map(({ action, kind, path }) => ({
-          action,
-          kind,
-          path
-        }))
-      ).eql([
-        {
-          action: 'scan',
-          kind: 'unknown',
-          path: mypath.join('/')
-        }
-      ])
-    })
-  })
-
-  describe('register an event on FS events', () => {
-    let syncPath
-    let producer
-
-    before(async () => {
-      syncPath = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'))
-      producer = new LinuxProducer({
-        syncPath
+      before(async () => {
+        syncPath = fs.mkdtempSync(path.join(os.tmpdir(), 'foo-'))
+        producer = new LinuxProducer({
+          syncPath
+        })
+        await producer.start()
+        await producer.buffer.pop()
       })
-      await producer.start()
-      await producer.buffer.pop()
-    })
 
-    it('detect events on folder in temp dir', async () => {
-      const dirname = 'foobaz'
-      const newname = 'foobarbaz'
-      fs.mkdirSync(path.join(syncPath, dirname))
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'created',
-          kind: 'directory',
-          path: dirname
-        }
-      ])
-      fs.renameSync(path.join(syncPath, dirname), path.join(syncPath, newname))
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'renamed',
-          kind: 'directory',
-          oldPath: dirname,
-          path: newname
-        }
-      ])
-      fs.rmdirSync(path.join(syncPath, newname))
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'deleted',
-          kind: 'directory',
-          path: newname
-        }
-      ])
-    })
+      it('detect events on folder in temp dir', async () => {
+        const dirname = 'foobaz'
+        const newname = 'foobarbaz'
+        fs.mkdirSync(path.join(syncPath, dirname))
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'created',
+            kind: 'directory',
+            path: dirname
+          }
+        ])
+        fs.renameSync(
+          path.join(syncPath, dirname),
+          path.join(syncPath, newname)
+        )
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'renamed',
+            kind: 'directory',
+            oldPath: dirname,
+            path: newname
+          }
+        ])
+        fs.rmdirSync(path.join(syncPath, newname))
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'deleted',
+            kind: 'directory',
+            path: newname
+          }
+        ])
+      })
 
-    it('detect events on file in temp dir', async () => {
-      const filename = 'barbaz'
-      const newname = 'barfoobaz'
-      const content = 'Hello, Cozy Drive for Desktop'
-      fs.writeFileSync(path.join(syncPath, filename), content)
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'created',
-          kind: 'file',
-          path: filename
-        },
-        {
-          action: 'modified',
-          kind: 'file',
-          path: filename
-        }
-      ])
-      fs.renameSync(path.join(syncPath, filename), path.join(syncPath, newname))
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'renamed',
-          kind: 'file',
-          oldPath: 'barbaz',
-          path: 'barfoobaz'
-        }
-      ])
-      fs.unlinkSync(path.join(syncPath, newname))
-      should(await producer.buffer.pop()).eql([
-        {
-          action: 'deleted',
-          kind: 'file',
-          path: newname
-        }
-      ])
+      it('detect events on file in temp dir', async () => {
+        const filename = 'barbaz'
+        const newname = 'barfoobaz'
+        const content = 'Hello, Cozy Drive for Desktop'
+        fs.writeFileSync(path.join(syncPath, filename), content)
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'created',
+            kind: 'file',
+            path: filename
+          },
+          {
+            action: 'modified',
+            kind: 'file',
+            path: filename
+          }
+        ])
+        fs.renameSync(
+          path.join(syncPath, filename),
+          path.join(syncPath, newname)
+        )
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'renamed',
+            kind: 'file',
+            oldPath: 'barbaz',
+            path: 'barfoobaz'
+          }
+        ])
+        fs.unlinkSync(path.join(syncPath, newname))
+        should(await producer.buffer.pop()).eql([
+          {
+            action: 'deleted',
+            kind: 'file',
+            path: newname
+          }
+        ])
+      })
     })
   })
-})
+}
 
 function mkdirSyncRecursive (syncPath = '/', mypath = '/') {
   mypath.split('/').reduce((acc, folder) => {
