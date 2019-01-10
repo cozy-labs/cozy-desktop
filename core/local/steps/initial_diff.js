de7441691d48f0dff3f1cb95675f2f6b3f0b3a71
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-01-10 15:40:19 +0100

    core/local/steps/initial_diff: Debounce events
      To avoid processing a `scan` event following the transformation of a
      `created` event into `renamed` event, we wait before passing it along
      to the next step and remove any `scan` event for the same path.

--- core/local/steps/initial_diff.js
+++ core/local/steps/initial_diff.js
@@ -3,27 +3,42 @@
 const { id } = require('../../metadata')
 const Buffer = require('./buffer')
 
-const logger = require('../../logger')
-const log = logger({
-  component: 'initialDiff'
-})
-
 /*::
 import type Pouch from '../../pouch'
-import type { Batch, EventKind } from './event'
+import type { AtomWatcherEvent, Batch, EventKind } from './event'
 import type { Metadata } from '../../metadata'
 
 type WatchedPath = {
   path: string,
   kind: EventKind,
 }
+
+type WaitingItem = {
+  batch: AtomWatcherEvent[],
+  nbCandidates: number,
+  timeout: TimeoutID
+}
 */
+
+// Wait this delay (in milliseconds) after the last event for a given file
+// before pushing this event to the next steps.
+// TODO tweak the value (the initial value was chosen because it looks like a
+//      good value, it is not something that was computed)
+const DELAY = 200
 
 // Some files and directories can have been deleted while cozy-desktop was
 // stopped. So, at the end of the initial scan, we have to do a diff between
 // what was in pouchdb and the events from the local watcher to find what was
 // deleted.
+module.exports = function (buffer /*: Buffer */, opts /*: { pouch: Pouch } */) /*: Buffer */ {
+  const out = new Buffer()
+  initialDiff(buffer, out, opts.pouch)
+  return out
+}
+
 async function initialDiff (buffer /*: Buffer */, out /*: Buffer */, pouch /*: Pouch */) /*: Promise<void> */ {
+  const waiting /*: WaitingItem[] */ = []
+
   // XXX we wait to receive the first batch of events before initializing this
   // component, as pouchdb may not be initialized when initialDiff is created
   // (its views are added later, but before the local watcher is started, thus
@@ -42,13 +57,11 @@ async function initialDiff (buffer /*: Buffer */, out /*: Buffer */, pouch /*: P
       byInode.set(doc.fileid || doc.ino, { path: doc.path, kind: kind })
     }
   }
-  let done = false
 
   while (true) {
-    if (done) {
-      out.push(events)
-      continue
-    }
+    let nbCandidates = 0
+
+    debounce(waiting, events)
 
     const batch /*: Batch */ = []
     for (const event of events) {
@@ -71,6 +84,7 @@ async function initialDiff (buffer /*: Buffer */, out /*: Buffer */, pouch /*: P
             // TODO for a directory, maybe we should check the children
             event.action = 'renamed'
             event.oldPath = was.path
+            nbCandidates++
           } else {
             // On linux, the inodes can have been reused: a file was deleted
             // and a directory created just after while the client was stopped
@@ -101,19 +115,57 @@ async function initialDiff (buffer /*: Buffer */, out /*: Buffer */, pouch /*: P
           })
         }
         byInode.clear()
-        done = true
       }
       batch.push(event)
     }
 
-    out.push(batch)
+    // Push the new batch of events in the queue
+    const timeout = setTimeout(() => {
+      out.push(waiting.shift().batch)
+      sendReadyBatches(waiting, out)
+    }, DELAY)
+    waiting.push({ batch, nbCandidates, timeout })
+
+    // Look if some batches can be sent without waiting
+    sendReadyBatches(waiting, out)
+
     events = await buffer.pop()
   }
 }
 
-module.exports = function (buffer /*: Buffer */, opts /*: { pouch: Pouch } */) /*: Buffer */ {
-  const out = new Buffer()
-  initialDiff(buffer, out, opts.pouch)
-    .catch(err => log.error({err}))
-  return out
+function sendReadyBatches (waiting /*: WaitingItem[] */, out /*: Buffer */) {
+  while (waiting.length > 0) {
+    if (waiting[0].nbCandidates !== 0) {
+      break
+    }
+    const item = waiting.shift()
+    clearTimeout(item.timeout)
+    if (item.batch.length > 0) {
+      out.push(item.batch)
+    }
+  }
+}
+
+// Look if we can debounce some waiting events with the current events
+function debounce (waiting /*: WaitingItem[] */, events /*: AtomWatcherEvent[] */) {
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i]
+    if (event.incomplete) {
+      continue
+    }
+    if (event.action === 'scan') {
+      for (let j = 0; j < waiting.length; j++) {
+        const w = waiting[j]
+        if (w.nbCandidates === 0) { continue }
+        for (let k = 0; k < w.batch.length; k++) {
+          const e = w.batch[k]
+          if (e.action === 'renamed' && e.path === event.path) {
+            events.splice(i, 1)
+            w.nbCandidates--
+            break
+          }
+        }
+      }
+    }
+  }
 }
