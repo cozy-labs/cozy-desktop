d392110d3c1f310cdb256b988bafe31efc3cf004
Enguerran <enguerran@users.noreply.github.com>
2019-01-18 10:39:44 +0100

    test: Cover await_write_finish that debounces events (#1402)
    We agreed on merging this PR and do some refactoring later to use Buffer#isEmpty().
    
    Heuristic to check emptyness:
    By adding a specific event at the end of the queue
    and by checking this is the event we receive when
    we pop() the last event, we can admit this is a
    heuristic that check the buffer is empty.

---
+++ test/unit/local/steps/await_write_finish.js
@@ -0,0 +1,141 @@
+/* eslint-env mocha */
+/* @flow */
+
+const should = require('should')
+const awaitWriteFinish = require('../../../../core/local/steps/await_write_finish')
+const Buffer = require('../../../../core/local/steps/buffer')
+
+const lastEventToCheckEmptyness = {
+  action: 'initial-scan-done',
+  kind: 'unknown',
+  path: ''
+}
+
+async function heuristicIsEmpty (buffer) {
+  const expected = await buffer.pop()
+  return (
+    (expected.length === 1 &&
+      Object.keys(expected[0]).reduce(
+        (acc, prop) =>
+          acc && expected[0][prop] === lastEventToCheckEmptyness[prop],
+        true
+      )) ||
+    console.log(expected)
+  )
+}
+
+describe('core/local/steps/await_write_finish', () => {
+  it('should reduce created→deleted to empty', async () => {
+    const buffer = new Buffer()
+    const originalBatch = [
+      {
+        action: 'created',
+        kind: 'file',
+        path: __filename
+      },
+      {
+        action: 'deleted',
+        kind: 'file',
+        path: __filename
+      },
+      lastEventToCheckEmptyness
+    ]
+    originalBatch.forEach(event => {
+      buffer.push([Object.assign({}, event)])
+    })
+    const enhancedBuffer = awaitWriteFinish(buffer, {})
+    should(await heuristicIsEmpty(enhancedBuffer)).be.true()
+  })
+
+  describe('created→modified→modified with or without deleted', () => {
+    it('should reduce created→modified→modified to created', async () => {
+      const buffer = new Buffer()
+      const originalBatch = [
+        {
+          action: 'created',
+          kind: 'file',
+          path: __filename
+        },
+        {
+          action: 'modified',
+          kind: 'file',
+          path: __filename
+        },
+        {
+          action: 'modified',
+          kind: 'file',
+          path: __filename
+        },
+        lastEventToCheckEmptyness
+      ]
+      originalBatch.forEach(event => {
+        buffer.push([Object.assign({}, event)])
+      })
+      const enhancedBuffer = awaitWriteFinish(buffer, {})
+      should(await enhancedBuffer.pop()).eql([originalBatch[0]])
+      should(await heuristicIsEmpty(enhancedBuffer)).be.true()
+    })
+
+    it('should reduce created→modified→modified→deleted to empty', async () => {
+      const buffer = new Buffer()
+      const originalBatch = [
+        {
+          action: 'created',
+          kind: 'file',
+          path: __filename
+        },
+        {
+          action: 'modified',
+          kind: 'file',
+          path: __filename
+        },
+        {
+          action: 'modified',
+          kind: 'file',
+          path: __filename
+        },
+        {
+          action: 'deleted',
+          kind: 'file',
+          path: __filename
+        },
+        lastEventToCheckEmptyness
+      ]
+      originalBatch.forEach(event => {
+        buffer.push([Object.assign({}, event)])
+      })
+      const enhancedBuffer = awaitWriteFinish(buffer, {})
+      should(await heuristicIsEmpty(enhancedBuffer)).be.true()
+    })
+  })
+
+  it('should not squash incomplete events', async () => {
+    const buffer = new Buffer()
+    const originalBatch = [
+      {
+        action: 'created',
+        kind: 'file',
+        path: __filename
+      },
+      {
+        action: 'modified',
+        kind: 'file',
+        path: __filename,
+        incomplete: true
+      },
+      {
+        action: 'modified',
+        kind: 'file',
+        path: __filename
+      },
+      lastEventToCheckEmptyness
+    ]
+    originalBatch.forEach(event => {
+      buffer.push([Object.assign({}, event)])
+    })
+    const enhancedBuffer = awaitWriteFinish(buffer, {})
+    should(await enhancedBuffer.pop()).eql([originalBatch[1]])
+    should(await enhancedBuffer.pop()).eql([originalBatch[0]])
+    should(await heuristicIsEmpty(enhancedBuffer)).be.true()
+  })
+})
