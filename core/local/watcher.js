595ba8e285575a224e3fc55ac95ea7513d6b7108
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-02-04 16:50:44 +0100

    core/local/watcher: Use Atom watcher by default
      For testing purposes we enable the new Atom watcher by default again
      on Linux and Windows.

--- core/local/watcher.js
+++ core/local/watcher.js
@@ -35,7 +35,7 @@ function platformDefaultWatcherType (platform /*: string */) /*: WatcherType */ 
   if (platform === 'darwin') {
     return 'chokidar'
   }
-  return 'chokidar' // TODO: Use atom watcher
+  return 'atom'
 }
 
 function build (syncPath /*: string */, prep /*: Prep */, pouch /*: Pouch */, events /*: EventEmitter */, ignore /*: Ignore */) /*: Watcher */ {
