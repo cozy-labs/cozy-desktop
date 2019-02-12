4b9edc3e9148d9831c83c3f77148f640871d1fd6
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-02-12 09:16:09 +0100

    dev/capture/local: Add saveFSEventsToFile() subdir option
    Default to 'local' so it still works with chokidar capture code.

--- dev/capture/local.js
+++ dev/capture/local.js
@@ -63,10 +63,10 @@ const isDone = (relpath) => {
   return relpath === DONE_FILE
 }
 
-const saveFSEventsToFile = (scenario, events) => {
+const saveFSEventsToFile = (scenario, events, subdir = 'local') => {
   const json = JSON.stringify(events, null, 2)
   const eventsFile = scenario.path
-    .replace(/scenario\.js/, path.join('local', `${process.platform}.json`))
+    .replace(/scenario\.js/, path.join(subdir, `${process.platform}.json`))
 
   return fse.outputFile(eventsFile, json)
     .then(() => path.basename(eventsFile))
