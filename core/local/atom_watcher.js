32d2c00c4fb827c06d96be2632ed71cab3743d42
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-02-18 17:53:08 +0100

    core/local/atom_watcher: Extract .steps and .producer()
    - Easier to see steps common to both platforms
    - Makes the constructor shorter

--- core/local/atom_watcher.js
+++ core/local/atom_watcher.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 const Promise = require('bluebird')
+const _ = require('lodash')
 
 const checksumer = require('./checksumer')
 const logger = require('../logger')
@@ -30,6 +31,36 @@ const log = logger({
   component: 'AtomWatcher'
 })
 
+/** Returns the step only when the given platform matches the current one.
+ *
+ * Makes it easy to include a step only for some platform.
+ */
+const only = (platform, step) => platform === process.platform && step
+
+/** The steps for the current platform. */
+const steps =
+  _.compact([
+    addInfos,
+    filterIgnored,
+    only('win32', winDetectMove),
+    scanFolder,
+    awaitWriteFinish,
+    initialDiff,
+    addChecksum,
+    incompleteFixer
+  ])
+
+/** The producer for the current platform. */
+const producer = opts => {
+  if (process.platform === 'linux') {
+    return new LinuxProducer(opts)
+  } else if (process.platform === 'win32') {
+    return new WinProducer(opts)
+  } else {
+    throw new Error('The experimental watcher is not available on this platform')
+  }
+}
+
 module.exports = class AtomWatcher {
   /*::
   syncPath: string
@@ -51,23 +82,11 @@ module.exports = class AtomWatcher {
     this.events = events
     this.ignore = ignore
     this.checksumer = checksumer.init()
-
+    this.producer = producer({syncPath})
     // Here, we build a chain of steps. Each step can be seen as an actor that
     // communicates with the next one via a buffer. The first step is called
     // the producer: even if the chain is ready at the end of this constructor,
     // the producer won't start pushing batches of events until it is started.
-    let steps
-    if (process.platform === 'linux') {
-      this.producer = new LinuxProducer(this)
-      steps = [addInfos, filterIgnored, scanFolder,
-        awaitWriteFinish, initialDiff, addChecksum, incompleteFixer]
-    } else if (process.platform === 'win32') {
-      this.producer = new WinProducer(this)
-      steps = [addInfos, filterIgnored, winDetectMove, scanFolder,
-        awaitWriteFinish, initialDiff, addChecksum, incompleteFixer]
-    } else {
-      throw new Error('The experimental watcher is not available on this platform')
-    }
     let buffer = steps.reduce((buf, step) => step(buf, this), this.producer.buffer)
     dispatch(buffer, this)
   }
