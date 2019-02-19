b049521f6c76eda1263db61f6333ea8806f39402
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-02-19 12:36:47 +0100

    core/local/steps/scan_folder: Use scan() directly
    Instead of depending on the whole Producer interface.

--- core/local/steps/scan_folder.js
+++ core/local/steps/scan_folder.js
@@ -7,7 +7,7 @@ const log = logger({
 
 /*::
 import type Buffer from './buffer'
-import type { Producer } from './producer'
+import type { Scanner } from './producer'
 */
 
 module.exports = {
@@ -18,14 +18,14 @@ module.exports = {
 // atom/Watcher emits a single event for the added directory. In this step,
 // when this happens, we scan the directory to see if it contains files and
 // sub-directories.
-function loop (buffer /*: Buffer */, opts /*: { producer: Producer } */) /*: Buffer */ {
+function loop (buffer /*: Buffer */, opts /*: { scan: Scanner } */) /*: Buffer */ {
   return buffer.asyncMap(async (batch) => {
     for (const event of batch) {
       if (event.incomplete) {
         continue
       }
       if (event.action === 'created' && event.kind === 'directory') {
-        opts.producer.scan(event.path)
+        opts.scan(event.path)
           .catch((err) => log.info({err, event}, 'Error on scan'))
       }
     }
