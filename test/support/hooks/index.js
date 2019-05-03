624a5dc02da569e725f7b3a222d6a45300d6f4a0
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-05-03 14:56:22 +0200

    test/support: Introduce ./hooks
    So we don't need to change `package.json` everytime we add a module
    there (otherwise it would make the `yarn mocha` script length quite
    big).

---
+++ test/support/hooks/index.js
@@ -0,0 +1,15 @@
+/** Load all global test hooks at once.
+ *
+ * Test support modules including global hooks (e.g. `before`, `beforeEach`,
+ * `after` or `afterEach` calls not inside a `describe` block) must be passed
+ * to mocha the same way as test files, not using `--require` / `mocha.opts`.
+ * Otherwise Node.js will complain about the missing hook functions.
+ *
+ * Hooks may be split into multiple files to improve readability / maintenance.
+ * But passing all of those files to the `mocha` command would be cumbersome.
+ * Instead, this module acts as a single entry-point for them.
+ *
+ * See the `yarn mocha` script.
+ */
+
+require('./logging')
