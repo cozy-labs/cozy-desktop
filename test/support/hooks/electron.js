36ac9c19e6e2d7a3380721256d602d2337f7a39c
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-05-03 10:01:22 +0200

    test/support/hooks: Load electron env with proxy

---
+++ test/support/hooks/electron.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+
+const { app, session } = require('electron')
+const setupProxy = require('../../../gui/js/proxy')
+
+before(done => {
+  setupProxy(app, session, () => {
+    done()
+  })
+})
