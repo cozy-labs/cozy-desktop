12cba5a288419db83324efa21fa7c690819e83d4
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-05-24 12:47:47 +0200

    test/scenarios: Rename .ods files update scenario
      We know LibreOffice can use `*.osl-tmp` temporary files when saving
      the new version of a document and that we need to do specific actions
      to manager those updates.
      We also want to ignore those temporary files altogether.
    
      By ignoring the temporary files, the scenario we wrote to test the
      update would still pass but would not be testing the actions we took
      in the first place to make it pass.
      Using a non ignored file as an intermediary file in the test makes
      sure we're still testing the right behaviour.

--- test/scenarios/update_through_unignored_tmp_file/scenario.js
+++ test/scenarios/update_through_unignored_tmp_file/scenario.js
@@ -6,10 +6,10 @@ module.exports = ({
   side: 'local',
   init: [{ ino: 1, path: 'file.ods', content: 'initial content' }],
   actions: [
-    { type: 'mv', src: 'file.ods', dst: 'file.ods.osl-tmp' },
+    { type: 'mv', src: 'file.ods', dst: 'other-file.ods' },
     { type: 'create_file', path: 'file.ods' },
     { type: 'update_file', path: 'file.ods', content: 'updated content #1' },
-    { type: 'delete', path: 'file.ods.osl-tmp' },
+    { type: 'delete', path: 'other-file.ods' },
     { type: 'update_file', path: 'file.ods', content: 'updated content #2' }
   ],
   expected: {
