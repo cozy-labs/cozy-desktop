53f40d28bffc3f0e2a1a0da807b57c48cae94003
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-05-24 13:00:06 +0200

    cozyignore: Ignore LibreOffice *.osl-tmp files
      Those files are temporary files used when saving the new version of a
      document.
      cf. https://cgit.freedesktop.org/libreoffice/core/tree/sal/osl/unx/file_misc.cxx?h=libreoffice-6-2-4#n819
    
      Their usage makes our job harder to detect the file update and we end
      up deleting the file altogether when there is no further update event
      after the temporary file is deleted.
    
      To mitigate the issue, we're ignoring those temporary files.
    
      We still need to find a more robust solution as we can't ignore every
      temporary file of every software.

---
+++ test/scenarios/ods_update_through_osl_tmp_file/scenario.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+/*:: import type { Scenario } from '..' */
+
+module.exports = ({
+  side: 'local',
+  init: [{ ino: 1, path: 'file.ods', content: 'initial content' }],
+  actions: [
+    { type: 'mv', src: 'file.ods', dst: 'file.ods.osl-tmp' },
+    { type: 'create_file', path: 'file.ods' },
+    { type: 'update_file', path: 'file.ods', content: 'updated content #1' },
+    { type: 'delete', path: 'file.ods.osl-tmp' }
+  ],
+  expected: {
+    tree: ['file.ods'],
+    remoteTrash: [],
+    contents: {
+      'file.ods': 'updated content #1'
+    }
+  }
+} /*: Scenario */)
