d9c3c83640fad30ee62cca3131d880c743b76dac
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-06-21 14:44:15 +0200

    test/scenarios: Move dir & replace subfile
    Same as Move dir & update subfile, but with different ino & size.

---
+++ test/scenarios/move_dir_and_replace_subfile/scenario.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+/*:: import type { Scenario } from '..' */
+
+module.exports = ({
+  disabled: {
+    // FIXME: Only work on macOS with STOPPED_CLIENT.
+    'local/darwin': 'Generates conflicts',
+    remote: 'Broken on macOS only?'
+  },
+  init: [
+    { ino: 1, path: 'src/' },
+    { ino: 2, path: 'src/file', content: 'initial content' }
+  ],
+  actions: [
+    { type: 'mv', src: 'src', dst: 'dst' },
+    { type: 'wait', ms: 1500 },
+    { type: 'create_file', path: 'dst/file.tmp', content: 'new content' },
+    { type: 'trash', path: 'dst/file' },
+    { type: 'mv', src: 'dst/file.tmp', dst: 'dst/file' }
+  ],
+  expected: {
+    tree: ['dst/', 'dst/file'],
+    // FIXME: old file will end up in the trash with chokidar, not with atom.
+    // remoteTrash: [],
+    contents: {
+      'dst/file': 'new content'
+    }
+  }
+} /*: Scenario */)
