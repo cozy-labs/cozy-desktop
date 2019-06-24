cf17ea21ba2535859a6dccb6f5710d5310f1c550
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-06-24 09:05:08 +0200

    core/local/chokidar: Extract ./prepare_events from ./watcher

--- test/unit/local/chokidar/watcher.js
+++ test/unit/local/chokidar/watcher.js
@@ -139,26 +139,6 @@ describe('ChokidarWatcher Tests', function() {
     })
   })
 
-  describe('#oldMetadata()', () => {
-    it('resolves with the metadata whose id matches the event path', async function() {
-      const old = await builders.metadata().create()
-      const resultByEventType = {}
-      for (let type of ['add', 'addDir', 'change', 'unlink', 'unlinkDir']) {
-        resultByEventType[type] = await this.watcher.oldMetadata({
-          type,
-          path: old.path
-        })
-      }
-      should(resultByEventType).deepEqual({
-        add: old,
-        addDir: old,
-        change: old,
-        unlink: old,
-        unlinkDir: old
-      })
-    })
-  })
-
   describe('onAddFile', () => {
     if (process.env.APPVEYOR) {
       it('is unstable on AppVeyor')
