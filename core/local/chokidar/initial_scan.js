ef469fed5912c505f1db046fa4e8c7f7c30e599e
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-11-04 15:44:42 +0100

    Load docs in bulk in initil scans and migrations (#1749)
    When loading all documents in migrations or the initial scan of either
    local watchers, we end up using a low-level db iterator which seems to
    fail at parsing documents when they're too deeply nested (i.e. lots of
    `moveFrom` inside `moveFrom`, probably resulting from conflict loops).
    
    PouchDB uses a fallback mechanism when parsing JSON documents fails
    and uses the vuvuzela library instead (see
    https://github.com/nolanlawson/vuvuzela) which does not suffer from
    deeply nested documents parsing errors.
    
    By using the PouchDB's `allDocs()` method to get all the saved
    documents, we hope not to use the low-level iterator that's failing
    us.

--- core/local/chokidar/initial_scan.js
+++ core/local/chokidar/initial_scan.js
@@ -39,7 +39,7 @@ const detectOfflineUnlinkEvents = async (
 ) /*: Promise<{offlineEvents: Array<ChokidarEvent>, unappliedMoves: string[], emptySyncDir: boolean}> */ => {
   // Try to detect removed files & folders
   const events /*: Array<ChokidarEvent> */ = []
-  const docs = await pouch.byRecursivePathAsync('')
+  const docs = await pouch.allDocs()
   const inInitialScan = doc =>
     initialScan.ids.indexOf(metadata.id(doc.path)) !== -1
 
