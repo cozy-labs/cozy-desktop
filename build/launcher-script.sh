f81063ea2f10dde41bf10cf2c20f3201b311a3b2
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-11-15 17:28:57 +0100

    launcher: Don't bail out on bash errors
      To determine whether unprivileged usernamespaces are enabled in the
      user's kernel or not, we check the content of a file that will
      probably be missing in most kernels where unprivileged usernamespaces
      have not been disabled by default.
      This will trigger an error that we send to `/dev/null` but the `-e`
      option of the launcher script forces the script to stop anyway.
    
      Now that the script has been implemented and tested, we can remove
      this option as well as the `-x` option which prints out every single
      command.

--- build/launcher-script.sh
+++ build/launcher-script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -ex
+
+# Uncomment for debugging purposes:
+# set -eux
 
 UNPRIVILEGED_USERNS_ENABLED=$(cat /proc/sys/kernel/unprivileged_userns_clone 2>/dev/null)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
