fc3fd58ceecb523537d2b0925877b62e4d7ad937
Erwan Guyader <erwan.guyader@cozycloud.cc>
2021-02-01 13:57:33 +0100

    gui/js/lastfiles: Persist same path only once (#2026)
    The list of recently synchronized files is persisted to disk in a JSON
    file. We only keep the last 250 files to keep it short.
    
    However, it happens that we would not remove existing occurrences of
    the same path when adding a file to the list although we only display
    the latest occurrence in the GUI. This means that if we were to
    synchronize 250 modifications on the same file. we would only display
    this one file in the GUI while the list on disk would be full.
    
    We now make sure only one occurrence is saved on disk.

---
+++ test/unit/gui/lastfiles.js
@@ -0,0 +1,67 @@
+/* @flow */
+/* eslint-env mocha */
+
+const should = require('should')
+const path = require('path')
+
+const lastfiles = require('../../../gui/js/lastfiles')
+
+const buildFile = fpath => ({
+  filename: path.basename(fpath),
+  path: fpath,
+  icon: 'file',
+  size: 0,
+  updated: +new Date()
+})
+
+describe('gui/js/lastfiles', () => {
+  beforeEach('clean lastfiles list', lastfiles.reset)
+
+  describe('add', () => {
+    it('adds the file to the list', async () => {
+      const file = buildFile('Administrative/Taxes/January.pdf')
+      await lastfiles.add(file)
+      await should(lastfiles.list()).be.fulfilledWith([file])
+    })
+
+    it('keeps only 250 files in the list', async () => {
+      let files = []
+      for (let n = 0; n < 250; n++) {
+        const file = buildFile(`Administrative/Taxes/January-${n}.pdf`)
+        files.push(file)
+        await lastfiles.add(file)
+      }
+
+      const file = buildFile(`Administrative/Taxes/January-250.pdf`)
+      await lastfiles.add(file)
+      await should(lastfiles.list()).be.fulfilledWith(
+        files.slice(1).concat(file)
+      )
+    })
+
+    it('removes files with the same path from the lsit', async () => {
+      const file = buildFile(`Administrative/Taxes/January.pdf`)
+      await lastfiles.add(file)
+
+      await lastfiles.add(file)
+      await should(lastfiles.list()).be.fulfilledWith([file])
+    })
+  })
+
+  describe('remove', () => {
+    it('removes all files with file path from the list', async () => {
+      let files = []
+      for (let n = 0; n < 250; n++) {
+        const file = buildFile(`Administrative/Taxes/January-${n}.pdf`)
+        files.push(file)
+        await lastfiles.add(file)
+      }
+
+      const file = buildFile(`Administrative/Taxes/January-139.pdf`)
+      await lastfiles.remove(file)
+      await should(lastfiles.list()).be.fulfilledWith(
+        files.slice(0, 139).concat(files.slice(140))
+      )
+    })
+  })
+})
