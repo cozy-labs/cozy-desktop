2115474cf4fe069c5f8ef0783dfec89b8e3eacf6
Erwan Guyader <erwan.guyader@cozycloud.cc>
2021-05-11 12:58:50 +0200

    gui: Always split paths using platform separator (#2090)
    When rendering a file in the Recent list, we split the given file path
    with the platform separator to get its name.
    However, we were using the `/` character to split the path in a user
    action message resulting in complete paths being displayed to Windows
    users instead of the base file/folder name.
    
    We'll now use the platform separator in this case as well.

--- gui/elm/Window/Tray/Dashboard.elm
+++ gui/elm/Window/Tray/Dashboard.elm
@@ -5,13 +5,13 @@ module Window.Tray.Dashboard exposing
     , maxActivities
     , nbActivitiesPerPage
     , renderFile
-    , samePath
     , showMoreButton
     , update
     , view
     )
 
 import Data.File as File exposing (EncodedFile, File)
+import Data.Path as Path exposing (Path)
 import Data.Platform as Platform exposing (Platform)
 import Data.UserAction as UserAction exposing (UserAction)
 import Html exposing (..)
@@ -63,8 +63,8 @@ maxActivities =
 type Msg
     = Transfer EncodedFile
     | Remove EncodedFile
-    | OpenPath String
-    | ShowInParent String
+    | OpenPath Path
+    | ShowInParent Path
     | Tick Time.Posix
     | ShowMore
     | Reset
@@ -81,11 +81,11 @@ update msg model =
         Transfer encodedFile ->
             let
                 file =
-                    File.decode encodedFile
+                    File.decode model.platform encodedFile
 
                 files =
                     file
-                        :: List.filter (samePath file >> not) model.files
+                        :: List.filter (File.samePath file >> not) model.files
                         |> List.take maxActivities
             in
             ( { model | files = files }, Cmd.none )
@@ -93,18 +93,18 @@ update msg model =
         Remove encodedFile ->
             let
                 file =
-                    File.decode encodedFile
+                    File.decode model.platform encodedFile
 
                 files =
-                    List.filter (samePath file >> not) model.files
+                    List.filter (File.samePath file >> not) model.files
             in
             ( { model | files = files }, Cmd.none )
 
         OpenPath path ->
-            ( model, Ports.openFile path )
+            ( model, Ports.openFile (Path.toString path) )
 
         ShowInParent path ->
-            ( model, Ports.showInParent path )
+            ( model, Ports.showInParent (Path.toString path) )
 
         Tick now ->
             ( { model | now = now }, Cmd.none )
@@ -138,9 +138,6 @@ update msg model =
 renderFile : Helpers -> Model -> File -> Html Msg
 renderFile helpers model file =
     let
-        pathSeparator =
-            Platform.pathSeparator model.platform
-
         ( basename, extname ) =
             File.splitName file.filename
 
@@ -148,18 +145,18 @@ renderFile helpers model file =
             helpers.distance_of_time_in_words file.updated model.now
 
         dirPath =
-            File.dirPath pathSeparator file.path file.filename
+            Path.parent file.path
 
         filenameTitle =
             Locale.interpolate [ file.filename ] <|
                 helpers.t "Dashboard Open file {0}"
 
         dirPathTitle =
-            if dirPath == pathSeparator then
+            if Path.isRoot dirPath then
                 helpers.t "Dashboard Show in parent folder"
 
             else
-                Locale.interpolate [ dirPath ] <|
+                Locale.interpolate [ Path.toString dirPath ] <|
                     helpers.t "Dashboard Show in folder {0}"
     in
     div
@@ -181,7 +178,7 @@ renderFile helpers model file =
                     Json.map (\msg -> ( msg, True )) <|
                         Json.succeed (ShowInParent file.path)
                 ]
-                [ text dirPath ]
+                [ text (Path.toString dirPath) ]
             ]
         ]
 
@@ -198,18 +195,18 @@ showMoreButton helpers =
         ]
 
 
-viewActions : Helpers -> List UserAction -> Html Msg
-viewActions helpers userActions =
-    case userActions of
+viewActions : Helpers -> Model -> Html Msg
+viewActions helpers model =
+    case model.userActions of
         action :: _ ->
-            viewAction helpers action
+            viewAction helpers model action
 
         _ ->
             Html.text ""
 
 
-viewAction : Helpers -> UserAction -> Html Msg
-viewAction helpers action =
+viewAction : Helpers -> Model -> UserAction -> Html Msg
+viewAction helpers model action =
     let
         title =
             UserAction.title action
@@ -225,7 +222,7 @@ viewAction helpers action =
                         Locale.interpolate chains string
                     )
                 |> List.map helpers.capitalize
-                |> List.map viewActionContentLine
+                |> List.map (viewActionContentLine model)
                 |> List.intersperse [ br [] [] ]
                 |> List.concat
 
@@ -304,7 +301,7 @@ view helpers model =
             List.take nbFiles model.files
     in
     section [ class "two-panes__content two-panes__content--dashboard" ]
-        [ viewActions helpers model.userActions
+        [ viewActions helpers model
         , div [ class "recent-files" ]
             (List.map renderLine filesToRender
                 ++ (if List.length model.files > nbFiles then
@@ -321,11 +318,6 @@ view helpers model =
 --HELPERS
 
 
-samePath : File -> File -> Bool
-samePath a b =
-    a.path == b.path
-
-
 removeCurrentAction : Model -> Model
 removeCurrentAction model =
     { model
@@ -340,11 +332,12 @@ currentUserAction model =
     List.head model.userActions
 
 
-viewActionContentLine : String -> List (Html Msg)
-viewActionContentLine line =
+viewActionContentLine : Model -> String -> List (Html Msg)
+viewActionContentLine { platform } line =
     let
         decorated =
-            Regex.find decorationRegex line
+            decoratedStrings line
+                |> List.map (Maybe.map (Path.fromString platform))
                 |> List.map decoratedName
 
         rest =
@@ -354,6 +347,14 @@ viewActionContentLine line =
     List.intersperseList decorated rest
 
 
+decoratedStrings : String -> List (Maybe String)
+decoratedStrings line =
+    Regex.find decorationRegex line
+        |> List.map .submatches
+        |> List.map List.head
+        |> List.map (Maybe.withDefault Nothing)
+
+
 decorationRegex : Regex.Regex
 decorationRegex =
     -- Find all groups of characters between backticks
@@ -361,34 +362,16 @@ decorationRegex =
         Regex.fromString "`(.+?)`"
 
 
-decoratedName : Regex.Match -> Html Msg
-decoratedName match =
-    let
-        path =
-            List.head match.submatches
-    in
+decoratedName : Maybe Path -> Html Msg
+decoratedName path =
     case path of
-        Just (Just str) ->
-            viewName str
+        Just p ->
+            span
+                [ class "u-bg-frenchPass u-bdrs-4 u-ph-half u-pv-0 u-c-pointer"
+                , title (Path.toString p)
+                , onClick (ShowInParent p)
+                ]
+                [ text (Path.name p) ]
 
         _ ->
             text ""
-
-
-viewName : String -> Html Msg
-viewName path =
-    span
-        [ class "u-bg-frenchPass u-bdrs-4 u-ph-half u-pv-0 u-c-pointer"
-        , title path
-        , onClick (ShowInParent path)
-        ]
-        [ text (shortName path) ]
-
-
-shortName : String -> String
-shortName long =
-    String.split "/" long
-        |> List.filter (not << String.isEmpty)
-        |> List.reverse
-        |> List.head
-        |> Maybe.withDefault ""
