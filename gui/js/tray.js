9f82e1ea50301fa1744ccc71ebb93ef5e7ef1aff
Erwan Guyader <erwan.guyader@cozycloud.cc>
2023-05-02 19:13:25 +0200

    feat: Limit system tray updates
      Updating the system tray icon and tooltip too often will lead to high
      CPU usage without any real gain for the user so we'll throttle these
      updates.
    
      We make sure we don't have more than 1 update every 5 s while still
      enforcing the first and last updates of a series as they're the most
      meaningful ones.

--- gui/js/tray.js
+++ gui/js/tray.js
@@ -6,9 +6,10 @@
 const { Tray, Menu, MenuItem, nativeImage } = require('electron')
 const { translate } = require('./i18n')
 const path = require('path')
+const _ = require('lodash')
 
 let tray = null
-let lastIconName = null
+let lastStatus = ''
 
 const imgs = path.resolve(__dirname, '..', 'images')
 const isMac = process.platform === 'darwin'
@@ -38,11 +39,92 @@ const setImage = iconName => {
     const pressedIcon = platformIcon(iconName, { pressed: true })
     tray.setPressedImage(pressedIcon)
   }
+}
 
-  lastIconName = iconName
+const systrayInfo = (appStatus, label) => {
+  switch (appStatus) {
+    case 'error':
+      return ['error', label]
+    case 'user-alert':
+      return ['pause', label]
+    case 'syncing':
+      return [
+        'sync',
+        translate('Tray Sync in progress') + (label ? ` ‟${label}“` : '…')
+      ]
+    case 'up-to-date':
+    case 'online':
+      return ['idle', translate('Tray Your cozy is up to date')]
+    case 'offline':
+      return ['offline', translate('Tray Offline')]
+    default:
+      return ['idle', '']
+  }
+}
+
+const setStatus = _.throttle(
+  (appStatus, label) => {
+    const [iconName, tooltip] = systrayInfo(appStatus, label)
+    const status = `${iconName}:${label}`
+
+    if (lastStatus != status) {
+      setImage(iconName)
+      tray.setToolTip(tooltip)
+      lastStatus = status
+    }
+  },
+  5000, // no more than one update per 5 seconds
+  { leading: true, trailing: true } // execute first and last upades
+)
+
+const init = (app, listener) => {
+  tray = new Tray(nativeImage.createEmpty())
+
+  // XXX: updating the status should always come after the initialization of the
+  // `tray` as we use it.
+  setStatus('idle', 'loading')
+
+  app.on('before-quit', () => tray.destroy())
+
+  let cachedBounds = null
+  const clicked = (e, bounds) => {
+    cachedBounds = bounds && bounds.y !== 0 ? bounds : cachedBounds
+    listener(tray.getBounds ? tray.getBounds() : cachedBounds)
+  }
+
+  // On Linux systems without libappindicator-gtk3 or other systems, clicks on
+  // the systray icon trigger events that can be caught to display the app
+  // window for example.
+  tray.on('click', clicked)
+  tray.on('right-click', clicked)
+  tray.on('double-click', clicked)
+
+  if (!isMac) {
+    // When click events are not triggered, we need to display a context menu so
+    // users can open the app's window.
+    const cm = Menu.buildFromTemplate([
+      { label: translate('Tray Quit application'), click: app.quit }
+    ])
+    cm.insert(
+      0,
+      new MenuItem({
+        label: translate('Tray Show application'),
+        click: clicked
+      })
+    )
+    cm.insert(1, new MenuItem({ type: 'separator' }))
+    tray.setContextMenu(cm)
+  }
+
+  setStatus('idle')
+}
+
+const wasInitiated = () => {
+  return tray != null
 }
 
 // old tray menu
+// TODO: reuse or remove
 /*
 const goToTab = (tab) => {
   const alreadyShown = !!mainWindow
@@ -84,77 +166,6 @@ if (newReleaseAvailable) {
 }
 tray.setContextMenu(menu)
 */
-
-const systrayInfo = (status, label) => {
-  switch (status) {
-    case 'error':
-      return ['error', label]
-    case 'user-alert':
-      return ['pause', label]
-    case 'syncing':
-      return [
-        'sync',
-        translate('Tray Sync in progress') + (label ? ` ‟${label}“` : '…')
-      ]
-    case 'up-to-date':
-    case 'online':
-      return ['idle', translate('Tray Your cozy is up to date')]
-    case 'offline':
-      return ['offline', translate('Tray Offline')]
-    default:
-      return ['idle', '']
-  }
-}
-
-const setStatus = (status, label) => {
-  const [iconName, tooltip] = systrayInfo(status, label)
-  tray.setToolTip(tooltip)
-
-  if (lastIconName !== iconName) setImage(iconName)
-}
-
-const init = (app, listener) => {
-  tray = new Tray(nativeImage.createEmpty())
-  setImage('idle')
-
-  app.on('before-quit', () => tray.destroy())
-
-  let cachedBounds = null
-  const clicked = (e, bounds) => {
-    cachedBounds = bounds && bounds.y !== 0 ? bounds : cachedBounds
-    listener(tray.getBounds ? tray.getBounds() : cachedBounds)
-  }
-
-  // On Linux systems without libappindicator-gtk3 or other systems, clicks on
-  // the systray icon trigger events that can be caught to display the app
-  // window for example.
-  tray.on('click', clicked)
-  tray.on('right-click', clicked)
-  tray.on('double-click', clicked)
-  tray.setToolTip('loading')
-
-  if (!isMac) {
-    // When click events are not triggered, we need to display a context menu so
-    // users can open the app's window.
-    const cm = Menu.buildFromTemplate([
-      { label: translate('Tray Quit application'), click: app.quit }
-    ])
-    cm.insert(
-      0,
-      new MenuItem({
-        label: translate('Tray Show application'),
-        click: clicked
-      })
-    )
-    cm.insert(1, new MenuItem({ type: 'separator' }))
-    tray.setContextMenu(cm)
-  }
-  setStatus('idle')
-}
-
-const wasInitiated = () => {
-  return tray != null
-}
 
 module.exports = {
   init,
